753c67b5283cd6ecfba75fc3acd9cd9fb0ac355a
srmathur <srmathur@8639bca7-a847-0410-9c84-802de9f82a5b>
2008-10-10 19:02:28 +0000

    write face flux to in fluent data file

--- src/fvm/scripts/testFlowModel.py
+++ src/fvm/scripts/testFlowModel.py
@@ -8,7 +8,7 @@ import exporters_atyped_double as exporters
 from FluentCase import FluentCase
 #fvmbaseExt.enableDebug("cdtor")
 
-reader = FluentCase("/home/sm/a/data/cav10.cas")
+reader = FluentCase("/home/sm/a/data/case1.cas")
 
 #import debug
 reader.read();
@@ -32,17 +32,18 @@ fmodel.init()
 momSolver = fmodel.getMomentumSolver()
 momSolver.relativeTolerance = 1e-1
 momSolver.nMaxCycles = 20
-momSolver.verbosity = 0
+momSolver.verbosity = 1
 
 contSolver = fmodel.getContinuitySolver()
 contSolver.relativeTolerance = 1e-1
 contSolver.nMaxCycles = 100
-contSolver.verbosity=0
-
-fmodel.advance(100)
+contSolver.verbosity=2
+contSolver.maxCoarseLevels=1
+fmodel.advance(1)
 
 writer = exporters.FluentDataExporterA(reader,"test.dat",False,0)
 writer.init()
 writer.writeScalarField(flowFields.pressure,1)
 writer.writeVectorField(flowFields.velocity,111)
+writer.writeScalarField(flowFields.massFlux,18)
 writer.finish()
--- src/fvm/scripts/testimport.py
+++ src/fvm/scripts/testimport.py
@@ -1,16 +1,38 @@
+import sys
+sys.setdlopenflags(0x100|0x2)
+
 import fvmbaseExt
 import importers
+import models_atyped_double as models
+import exporters_atyped_double as exporters
+from FluentCase import FluentCase
+#fvmbaseExt.enableDebug("cdtor")
 
-fvmbaseExt.enableDebug("cdtor")
+reader = FluentCase("/home/sm/olda/data/cond.cas")
 
-reader = importers.FluentReader("/home/sm/uvp.cas")
-
-import debug
-reader.readMesh();
+#import debug
+reader.read();
 
 
 meshes = reader.getMeshList()
 
-metricsCalculator = fvmbaseExt.MeshMetricsCalculator_double(meshes)
+geomFields =  models.GeomFields('geom')
+metricsCalculator = models.MeshMetricsCalculatorA(geomFields,meshes)
 
 metricsCalculator.init()
+
+thermalFields =  models.ThermalFields('therm')
+
+tmodel = models.ThermalModelA(geomFields,thermalFields,meshes)
+bcmap = tmodel.getBCMap()
+bc2 = bcmap[2]
+
+reader.importThermalBCs(tmodel)
+tmodel.printBCs()
+tmodel.init()
+tmodel.advance(1)
+
+writer = exporters.FluentDataExporterA(reader,"test.dat",False,0)
+writer.init()
+writer.writeScalarField(thermalFields.temperature,3)
+writer.finish()
