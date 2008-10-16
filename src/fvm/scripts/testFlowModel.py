38c17ff58f19047a3a6e312d13191b3f04df50f1
srmathur <srmathur@8639bca7-a847-0410-9c84-802de9f82a5b>
2008-10-16 14:08:17 +0000

    controls for some more options

--- src/fvm/scripts/testFlowModel.py
+++ src/fvm/scripts/testFlowModel.py
@@ -5,16 +5,22 @@ import fvmbaseExt
 import importers
 import models_atyped_double as models
 import exporters_atyped_double as exporters
+#import models_atyped_tangent_double as models
+#import exporters_atyped_tangent_double as exporters
 from FluentCase import FluentCase
 #fvmbaseExt.enableDebug("cdtor")
 
-reader = FluentCase("/home/sm/a/data/case1.cas")
+fileBase = "/home/sm/prism-meshes/cav32"
+reader = FluentCase(fileBase+".cas")
 
 #import debug
 reader.read();
 
 
 meshes = reader.getMeshList()
+
+import time
+t0 = time.time()
 
 geomFields =  models.GeomFields('geom')
 metricsCalculator = models.MeshMetricsCalculatorA(geomFields,meshes)
@@ -27,21 +33,34 @@ fmodel = models.FlowModelA(geomFields,flowFields,meshes)
 
 reader.importFlowBCs(fmodel)
 #fmodel.printBCs()
-fmodel.init()
 
 momSolver = fmodel.getMomentumSolver()
 momSolver.relativeTolerance = 1e-1
 momSolver.nMaxCycles = 20
-momSolver.verbosity = 1
+momSolver.maxCoarseLevels=20
+momSolver.verbosity=0
 
 contSolver = fmodel.getContinuitySolver()
 contSolver.relativeTolerance = 1e-1
-contSolver.nMaxCycles = 100
-contSolver.verbosity=2
-contSolver.maxCoarseLevels=1
-fmodel.advance(1)
+contSolver.nMaxCycles = 20
+contSolver.verbosity=0
+contSolver.maxCoarseLevels=20
 
-writer = exporters.FluentDataExporterA(reader,"test.dat",False,0)
+foptions = fmodel.getOptions()
+foptions.momentumTolerance=1e-5
+foptions.continuityTolerance=1e-9
+foptions.setVar("momentumURF",0.95)
+foptions.setVar("pressureURF",0.1)
+foptions.printNormalizedResiduals=False
+
+fmodel.init()
+fmodel.advance(4000)
+
+t1 = time.time()
+
+print 'solution time = %f' % (t1-t0)
+
+writer = exporters.FluentDataExporterA(reader,fileBase+"-prism.dat",False,0)
 writer.init()
 writer.writeScalarField(flowFields.pressure,1)
 writer.writeVectorField(flowFields.velocity,111)
