6960e21741a949041cf99bd511c2fba8dca8feaa
srmathur <srmathur@8639bca7-a847-0410-9c84-802de9f82a5b>
2008-10-20 19:10:54 +0000

    support for running as standalone script with args

--- src/fvm/scripts/testFlowModel.py
+++ src/fvm/scripts/testFlowModel.py
@@ -1,16 +1,46 @@
+#!/usr/bin/env python
+
 import sys
 sys.setdlopenflags(0x100|0x2)
 
 import fvmbaseExt
 import importers
-import models_atyped_double as models
-import exporters_atyped_double as exporters
-#import models_atyped_tangent_double as models
-#import exporters_atyped_tangent_double as exporters
+
+atype = 'double'
+#atype = 'tangent'
+
+if atype == 'double':
+    import models_atyped_double as models
+    import exporters_atyped_double as exporters
+elif atype == 'tangent':
+    import models_atyped_tangent_double as models
+    import exporters_atyped_tangent_double as exporters
+
+
 from FluentCase import FluentCase
+
 #fvmbaseExt.enableDebug("cdtor")
 
-fileBase = "/home/sm/prism-meshes/cav32"
+fileBase = None
+numIterations = 10
+#fileBase = "/home/sm/prism-meshes/cav32"
+#fileBase = "/home/sm/a/data/wj"
+
+def usage():
+    print "Usage: %s filebase" % sys.argv[0]
+    print "Where filebase.cas is a Fluent case file."
+    print "Output will be in filebase-prism.dat"
+    sys.exit(1)
+
+# change as needed
+
+if __name__ == '__main__' and fileBase is None:
+    if len(sys.argv) != 2:
+        usage()
+
+    fileBase = sys.argv[1]
+
+
 reader = FluentCase(fileBase+".cas")
 
 #import debug
@@ -26,6 +56,9 @@ geomFields =  models.GeomFields('geom')
 metricsCalculator = models.MeshMetricsCalculatorA(geomFields,meshes)
 
 metricsCalculator.init()
+
+if atype == 'tangent':
+    metricsCalculator.setTangentCoords(0,7,1)
 
 flowFields =  models.FlowFields('flow')
 
@@ -47,14 +80,22 @@ contSolver.verbosity=0
 contSolver.maxCoarseLevels=20
 
 foptions = fmodel.getOptions()
-foptions.momentumTolerance=1e-5
-foptions.continuityTolerance=1e-9
-foptions.setVar("momentumURF",0.95)
-foptions.setVar("pressureURF",0.1)
+foptions.momentumTolerance=1e-3
+foptions.continuityTolerance=1e-3
+foptions.setVar("momentumURF",0.7)
+foptions.setVar("pressureURF",0.3)
 foptions.printNormalizedResiduals=False
 
+"""
+if atype=='tangent':
+    vcMap = fmodel.getVCMap()
+    for i,vc in vcMap.iteritems():
+        print vc.getVar('viscosity')
+        vc.setVar('viscosity',(1.7894e-5,1))
+"""
+
 fmodel.init()
-fmodel.advance(4000)
+fmodel.advance(numIterations)
 
 t1 = time.time()
 
@@ -66,3 +107,11 @@ writer.writeScalarField(flowFields.pressure,1)
 writer.writeVectorField(flowFields.velocity,111)
 writer.writeScalarField(flowFields.massFlux,18)
 writer.finish()
+
+if (atype=='tangent'):
+    writer = exporters.FluentDataExporterA(reader,fileBase+"-prism-tangent.dat",False,1)
+    writer.init()
+    writer.writeScalarField(flowFields.pressure,1)
+    writer.writeVectorField(flowFields.velocity,111)
+    writer.writeScalarField(flowFields.massFlux,18)
+    writer.finish()
