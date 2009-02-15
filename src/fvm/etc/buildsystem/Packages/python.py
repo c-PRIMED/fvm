19763280968c0924926f8b5989eee15021fc1023
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-02-15 18:54:56 +0000

    add numpy path for fvm

--- src/fvm/etc/buildsystem/Packages/python.py
+++ src/fvm/etc/buildsystem/Packages/python.py
@@ -1,10 +1,10 @@
 from distutils.sysconfig import get_config_vars, get_python_version, get_python_inc
-import os.path
+import numpy, os.path
 import Arch
 def generate(env):
     cv = get_config_vars()
     pylib = 'python'+get_python_version()
-    env.Append(CPPPATH=get_python_inc())
+    env.Append(CPPPATH=[get_python_inc(),numpy.get_include()])
     if Arch.isWindows():
         env.Append(LIBS=[pylib.replace(".","")])
         env.Append(LIBPATH=os.path.join(cv['prefix'],"libs"))
