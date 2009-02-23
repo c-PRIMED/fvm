85c1628f2b5aad6d8339842f0831728875b52c7c
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-02-23 19:04:20 +0000

    Main function for calling Parmesh

---
+++ src/fvm/src/modules/parallel/pFVM.cpp
@@ -0,0 +1,206 @@
+#include <string>
+#include <vector>
+
+#include "MMReader.h"
+#include "AMG.h"
+#include "Array.h"
+
+#include "PartMesh.h"
+#include "MPIProcess.h"
+#include "FluentReader.h"
+#include "Mesh.h"
+
+using namespace std;
+
+void  debug_mesh(MeshList mesh_list);
+
+
+int main(int argc, char *argv[])
+{
+/*  MMReader reader(argv[1], argv[2]);
+  
+  shared_ptr<LinearSystem> ls(reader.getLS());
+
+  AMG solver;
+  solver.solve(*ls); */
+
+
+   //MPI::Init(argc, argv);
+
+   //PartMesh*  partMesh = new PartMesh( MPI::COMM_WORLD.Get_rank() );
+
+   MPIProcess* proc = new MPIProcess(argc, argv);
+
+   string file_name( argv[1] );
+   FluentReader* fluent_reader = new FluentReader( file_name );
+
+   fluent_reader->readMesh();
+
+   MeshList mesh_list = fluent_reader->getMeshList();
+
+   //debug_mesh( mesh_list );
+
+
+  //mesh_list
+   vector<int> npart( mesh_list.size(), proc->totalProcs() );
+   vector<PartMesh::ETYPE> etype( mesh_list.size(), PartMesh::TRI);
+
+  //constructer to PartMesh
+   PartMesh part_mesh(mesh_list, npart, etype );
+
+  //set properties of Partition
+   part_mesh.setWeightType(PartMesh::NOWEIGHTS);
+   part_mesh.setNumFlag(PartMesh::C_STYLE);
+
+  //actions
+   part_mesh.partition();
+   part_mesh.dumpTecplot();	//there is bug for quadrilateral elements in this method find it after turkey
+
+   part_mesh.mesh();
+   part_mesh.mesh_debug();
+
+   delete fluent_reader;
+
+
+   //string  file_name(argv[1]);
+   //cout << " file-name = " << file_name << endl;
+
+   
+   delete proc;
+
+
+   return 0;
+}
+
+
+
+void  debug_mesh( MeshList mesh_list )
+{
+//understand Mesh 
+   Mesh*    mesh = mesh_list.at(0);
+
+   cout << " mesh dimension = " << mesh->getDimension() << endl;
+   cout << " mesh ID        = " << mesh->getID()        << endl;
+   
+   StorageSite&  faces = mesh->getFaces();
+   StorageSite&  cells = mesh->getCells();
+   StorageSite&  nodes = mesh->getNodes();
+
+   cout << " Faces " << endl;
+   cout << " StorageSite::getCount()     = " << faces.getCount() << endl;
+   cout << " StorageSite::getSelfCount() = " << faces.getSelfCount() << endl;
+   cout << " StorageSite::getOffset()    = " << faces.getOffset() << endl;
+
+   cout << " Cells " << endl;
+   cout << " StorageSite::getCount()     = " << cells.getCount() << endl;
+   cout << " StorageSite::getSelfCount() = " << cells.getSelfCount() << endl;
+   cout << " StorageSite::getOffset()    = " << cells.getOffset() << endl;
+
+   cout << " Nodes " << endl;
+   cout << " StorageSite::getCount()     = " << nodes.getCount() << endl;
+   cout << " StorageSite::getSelfCount() = " << nodes.getSelfCount() << endl;
+   cout << " StorageSite::getOffset()    = " << nodes.getOffset() << endl;
+
+{
+   const CRConnectivity&  cellNodes= mesh->getCellNodes();
+   //const CRConnectivity&  cellNodes= mesh->getCellCells();   
+   //const CRConnectivity&    cellNodes = mesh->getCellFaces();
+   //const CRConnectivity&   cellNodes = mesh->getAllFaceNodes();
+   const Array<int>&   rowCellNodes = cellNodes.getRow();
+   const Array<int>&   colCellNodes = cellNodes.getCol();
+   
+
+   cout << " CRConnectivity::getCellNodes() " << endl;
+   cout << " ::getRowDim() = " << cellNodes.getRowDim() << endl;
+   cout << " ::getColDim() = " << cellNodes.getColDim() << endl;
+   for ( int cell = 0; cell < cellNodes.getRowDim(); cell++){
+      cout << " row[" << cell+1 <<"] = " << rowCellNodes[cell] << "    ";
+      int nnodes = rowCellNodes[cell+1] - rowCellNodes[cell];
+      for ( int node = 0; node < nnodes; node++){
+         cout << colCellNodes[ rowCellNodes[cell] + node ]+1 << "    ";
+      }
+      cout << endl;
+   }
+}
+   cout << " " << endl;
+   cout << " Mesh::getFaceGroupCount()     = " << mesh->getFaceGroupCount() << endl;
+   cout << " Mesh::getBoundaryGroupCount() = " << mesh->getBoundaryGroupCount() << endl;
+   cout << " Mesh::getInterfaceGroupCount()= " << mesh->getInterfaceGroupCount() << endl;
+   cout << endl;
+   
+   const FaceGroup& interiorFaceGroup = mesh->getInteriorFaceGroup();
+   cout << " interiorFaceGroup id        = " << interiorFaceGroup.id << endl;
+   cout << " interiorFaceGroup groupType = " << interiorFaceGroup.groupType << endl; 
+   cout << " site.getCount()             = " << interiorFaceGroup.site.getCount() << endl;
+   cout << endl;
+   
+   const FaceGroupList&  bounFaceGroup = mesh->getBoundaryFaceGroups();
+
+  
+   
+   for ( int i = 0; i < int(bounFaceGroup.size()); i++){
+      cout << " bounFaceGroup id        = " << bounFaceGroup.at(i)->id << endl;
+      cout << " bounFaceGroup groupType = " << bounFaceGroup.at(i)->groupType << endl; 
+      cout << " site.getCount()             = " << bounFaceGroup.at(i)->site.getCount() << endl;
+      cout << endl;
+   }
+
+
+{
+  //use of CRConnectivity::getSubset
+  const CRConnectivity&  cellNodes= mesh->getCellNodes();
+  StorageSite site( 3, 2);
+  Array<int> indices(5);
+  indices[0] = 35; indices[1] = 3; indices[2] = 2; indices[3] = 16; indices[4] = 29;
+  shared_ptr<CRConnectivity> subCellNodes = cellNodes.getSubset( site, indices);
+  const Array<int>&   rowCellNodes = subCellNodes->getRow();
+  const Array<int>&   colCellNodes = subCellNodes->getCol();
+
+  cout << " subCellNodes = " << endl;
+  cout << " getRowDim()  = " << subCellNodes->getRowDim() << endl;
+  cout << " getColDim()  = " << subCellNodes->getColDim() << endl;
+  for ( int cell = 0; cell < subCellNodes->getRowDim(); cell++){
+      cout << " row[" << cell+1 <<"] = " << rowCellNodes[cell] << "    ";
+      int nnodes = rowCellNodes[cell+1] - rowCellNodes[cell];
+      for ( int node = 0; node < nnodes; node++){
+         cout << colCellNodes[ rowCellNodes[cell] + node ]+1 << "    ";
+      }
+      cout << endl;
+  }
+
+ cout << endl;
+}
+
+//use of CRConnectivity::getLocalizedSubset(..)
+{
+  const CRConnectivity&  cellNodes= mesh->getCellNodes();
+  StorageSite rowsite( 3, 2);
+  StorageSite colsite(13);
+  Array<int> indices(5);
+  indices[0] = 35; indices[1] = 3; indices[2] = 2; indices[3] = 16; indices[4] = 29;
+  shared_ptr<CRConnectivity> subLocalCellNodes = cellNodes.getLocalizedSubset( rowsite, colsite, indices);
+  const Array<int>&   rowCellNodes = subLocalCellNodes->getRow();
+  const Array<int>&   colCellNodes = subLocalCellNodes->getCol();
+
+  cout << " subCellNodes = " << endl;
+  cout << " getRowDim()  = " << subLocalCellNodes->getRowDim() << endl;
+  cout << " getColDim()  = " << subLocalCellNodes->getColDim() << endl;
+  for ( int cell = 0; cell < subLocalCellNodes->getRowDim(); cell++){
+      cout << " row[" << cell+1 <<"] = " << rowCellNodes[cell] << "    ";
+      int nnodes = rowCellNodes[cell+1] - rowCellNodes[cell];
+      for ( int node = 0; node < nnodes; node++){
+         cout << colCellNodes[ rowCellNodes[cell] + node ]+1 << "    ";
+      }
+      cout << endl;
+  }
+
+ cout << endl;
+
+}
+
+
+  
+   
+
+
+}
