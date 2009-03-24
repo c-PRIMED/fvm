50c3efc9131d00681d9bcee0e0ca4739005ed078
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-03-24 13:51:47 +0000

    scatterMap, and gatherMap has been added

--- src/fvm/src/modules/fvmbase/StorageSite.cpp
+++ src/fvm/src/modules/fvmbase/StorageSite.cpp
@@ -17,3 +17,29 @@ StorageSite::~StorageSite()
 {
   logDtorVerbose("of size %d" ,_count);
 }
+
+
+void
+StorageSite::scatterGatherMaps( )
+{
+
+       MappersMap::const_iterator it_mapper;
+       //loop over interfaces
+        for ( it_mapper = _mappers.begin(); it_mapper != _mappers.end(); it_mapper++ ){
+            const StorageSite *site = it_mapper->first;
+
+            int size = site->getCount();
+            shared_ptr< Array<int> > gather_values ( new Array<int> (size) );
+            shared_ptr< Array<int> > scatter_values( new Array<int> (size) );
+
+            const OneToOneIndexMap&  indexMap = *(it_mapper->second);
+            for ( int i = 0; i < size; i++){
+                 (*gather_values)[i]  = indexMap.getFromIndices()[i];
+                 (*scatter_values)[i] = indexMap.getToIndices()[i];
+            }
+            _scatterMap.insert( pair< const StorageSite*, shared_ptr< Array<int> > > ( site, scatter_values ) );
+            _gatherMap.insert ( pair< const StorageSite*, shared_ptr< Array<int> > > ( site, gather_values  ) );
+         }
+
+
+}
