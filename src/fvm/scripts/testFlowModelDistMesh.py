1aa5c170895ea3d5817796ffd927bb0e74c052ec
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-04-03 15:21:05 +0000

    tecplot writer added

--- src/fvm/scripts/testFlowModelDistMesh.py
+++ src/fvm/scripts/testFlowModelDistMesh.py
@@ -6,6 +6,8 @@ sys.setdlopenflags(0x100|0x2)
 import fvmbaseExt
 import importers
 import time
+from numpy import *
+
 
 atype = 'double'
 #atype = 'tangent'
@@ -39,6 +41,123 @@ def advance(fmodel,niter):
             fmodel.advance(1)
         except KeyboardInterrupt:
             break
+	    
+	    
+def dumpTecplotFile(nmesh, meshes):
+  #cell sites
+  cellSites = []
+  for n in range(0,nmesh):
+     cellSites.append( meshes[n].getCells() )
+     print "cellSites[", n, "].getCount = ", cellSites[n].getCount()
+
+  #face sites
+  faceSites = []
+  for n in range(0,nmesh):
+     faceSites.append( meshes[n].getFaces() )
+
+  #node sites
+  nodeSites = []
+  for n in range(0,nmesh):
+     nodeSites.append( meshes[n].getNodes() )
+
+  #get connectivity (faceCells)
+  faceCells = []
+  for n in range(0,nmesh):
+     faceCells.append( meshes[n].getConnectivity( faceSites[n], cellSites[n] ) )
+ 
+  #get connectivity ( cellNodes )
+  cellNodes = []
+  for n in range(0,nmesh):
+     cellNodes.append( meshes[n].getCellNodes() )
+
+  #get Volume as array
+  volumes = []
+  for n in range(0,nmesh):
+     volumes.append( geomFields.volume[cellSites[n]].asNumPyArray() )
+ 
+
+  velFields = []
+  for n in range(0,nmesh):
+     velFields.append( flowFields.velocity[cellSites[n]].asNumPyArray() )
+
+  coords = []
+  for n in range(0,nmesh):
+     coords.append( meshes[n].getNodeCoordinates().asNumPyArray() )
+     print "shape( coords[", n, "] ) = ", shape( coords[n] )	
+     
+
+  f = open("dist_mesh_tecplot.dat", 'w')
+
+  f.write("Title = \" tecplot file for 2D Cavity problem \" \n")
+  f.write("variables = \"x\", \"y\", \"z\", \"velX\", \"velY\", \"velZ\" \n")
+  for n in range(0,nmesh):
+     title_name = "nmesh" + str(n)
+     ncell  = cellSites[n].getSelfCount()
+     nnode  = nodeSites[n].getCount()
+     zone_name = "Zone T = " + "\"" + title_name +  "\"" +      \
+                 " N = " + str( nodeSites[n].getCount() ) +     \
+		 " E = " + str( ncell ) +  \
+		 " DATAPACKING = BLOCK, VARLOCATION = ([4-6]=CELLCENTERED), " + \
+		 " ZONETYPE=FETRIANGLE \n"
+     f.write( zone_name )
+     #write x
+     for i in range(0,nnode):
+          f.write(str(coords[n][i][0])+"    ")
+	  if ( i % 5 == 4 ):
+	     f.write("\n")
+     f.write("\n")	  
+     
+     #write y
+     for i in range(0,nnode):
+          f.write(str(coords[n][i][1])+"    ")
+	  if ( i % 5 == 4 ):
+	     f.write("\n")
+     f.write("\n")	  
+
+     #write z
+     for i in range(0,nnode):
+          f.write(str(coords[n][i][2])+"    ")
+	  if ( i % 5 == 4 ):
+	     f.write("\n")
+     f.write("\n")	  
+     
+     
+     #write velX
+     for i in range(0,ncell):
+        f.write( str(velFields[n][i][0]) + "    ")
+	if ( i % 5  == 4 ):
+	    f.write("\n")
+     f.write("\n")
+     	  	    
+     #write velY
+     for i in range(0,ncell):
+        f.write( str(velFields[n][i][1]) + "    ")
+	if ( i % 5  == 4 ):
+	    f.write("\n")
+     f.write("\n")	  
+     #write velZ
+     for i in range(0,ncell):
+        f.write( str(velFields[n][i][2]) + "    ")
+	if ( i % 5  == 4 ):
+	    f.write("\n")
+     f.write("\n")	    
+     #connectivity
+     for i in range(0,ncell):
+        nnodes_per_cell = cellNodes[n].getCount(i)
+        for node in range(0,nnodes_per_cell):
+	    f.write( str(cellNodes[n](i,node)+1) + "     ")
+        f.write("\n")
+
+     	    
+	    
+     f.write("\n")	  
+     
+	
+  
+  f.close()
+	    
+  
+      
 
 # change as needed
 #import debug
@@ -66,14 +185,12 @@ import time
 t0 = time.time()
 
 geomFields =  models.GeomFields('geom')
-print "before metricsCalculator"
 metricsCalculator = models.MeshMetricsCalculatorA(geomFields,meshes)
-print "after"
 metricsCalculator.init()
 
 if atype == 'tangent':
     metricsCalculator.setTangentCoords(0,7,1)
-print "before flowFields " 
+
 flowFields =  models.FlowFields('flow')
 
 fmodel = models.FlowModelA(geomFields,flowFields,meshes)
@@ -90,10 +207,6 @@ for vc in vcMap.values():
     vc.setVar('viscosity',1.0)
 
 
-print "before BCreader " 
-
-
-print "before Momentum solvers " 
 momSolver = fvmbaseExt.AMG()
 momSolver.relativeTolerance = 1e-1
 momSolver.nMaxIterations = 20
@@ -101,10 +214,6 @@ momSolver.maxCoarseLevels=20
 momSolver.verbosity=0
 
 contSolver = fvmbaseExt.AMG()
-#pc = fvmbaseExt.AMG()
-#pc.verbosity=0
-#contSolver = fvmbaseExt.BCGStab()
-#contSolver.preconditioner = pc
 contSolver.relativeTolerance = 1e-1
 contSolver.nMaxIterations = 20
 contSolver.verbosity=0
@@ -125,26 +234,17 @@ foptions.printNormalizedResiduals=False
 fmodel.init()
 #advance(fmodel,numIterations)
 
-c0 = meshes[0].getCells()
-c1 = meshes[1].getCells()
-c2 = meshes[2].getCells()
-c3 = meshes[3].getCells()
-
-f0 = meshes[0].getFaces()
-f1 = meshes[1].getFaces()
-f2 = meshes[2].getFaces()
-f3 = meshes[3].getFaces()
-
-fc0 = meshes[0].getConnectivity(f0,c0)
-fc1 = meshes[1].getConnectivity(f1,c1)
-fc2 = meshes[2].getConnectivity(f2,c2)
-fc3 = meshes[3].getConnectivity(f3,c3)
-
-
-vol0 = geomFields.volume[c0].asNumPyArray()
-vol1 = geomFields.volume[c1].asNumPyArray()
-vol2 = geomFields.volume[c2].asNumPyArray()
-vol3 = geomFields.volume[c3].asNumPyArray()
 
 #import debug
-fmodel.advance(100)
+fmodel.advance(1000)
+
+
+
+
+
+dumpTecplotFile(nmesh, meshes)
+
+
+
+
+
