ea2bb14dc7b113ceb93f81f2a47e575a1902c805
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-04-06 14:23:47 +0000

    comment out gsl tests

--- tools/build/build_packages.py
+++ tools/build/build_packages.py
@@ -269,31 +269,31 @@ class Gsl(BuildPkg):
         return self.sys_log("make install")
     def _clean(self):
         return self.sys_log("make clean")
-    def _test(self):
-        ok = errs = 0
-        os.chdir(self.bdir)
-        logfile = self.logfile.replace('xml','txt')
-        os.system("make check > %s 2>&1" % logfile)
-        for line in open(logfile):
-            if line.find('PASS') == 0:
-                ok += 1
-            elif line.find('FAIL') == 0:
-                errs += 1
-        if errs:
-            ostr = "<Test Status=\"failed\">\n"
-        else:
-            ostr = "<Test Status=\"passed\">\n"
-        ostr += "\t<Name>gsl</Name>\n"
-        ostr += "\t<Path>%s</Path>\n" % self.sdir
-        ostr += "\t<FullName>gsl</FullName>\n"
-        ostr += "\t<FullCommandLine>make check</FullCommandLine>\n"
-        ostr += "\t<Results><Measurement><Value>"
-        ostr += cgi.escape(open(logfile).read())
-        ostr += "</Value></Measurement></Results></Test>\n"
-        f = open(self.logfile,'w')
-        f.write(ostr)
-        f.close()
-        return ok, errs
+#    def _test(self):
+#        ok = errs = 0
+#        os.chdir(self.bdir)
+#        logfile = self.logfile.replace('xml','txt')
+#        os.system("make check > %s 2>&1" % logfile)
+#        for line in open(logfile):
+#            if line.find('PASS') == 0:
+#                ok += 1
+#            elif line.find('FAIL') == 0:
+#                errs += 1
+#        if errs:
+#            ostr = "<Test Status=\"failed\">\n"
+#        else:
+#            ostr = "<Test Status=\"passed\">\n"
+#        ostr += "\t<Name>gsl</Name>\n"
+#        ostr += "\t<Path>%s</Path>\n" % self.sdir
+#        ostr += "\t<FullName>gsl</FullName>\n"
+#        ostr += "\t<FullCommandLine>make check</FullCommandLine>\n"
+#        ostr += "\t<Results><Measurement><Value>"
+#        ostr += cgi.escape(open(logfile).read())
+#        ostr += "</Value></Measurement></Results></Test>\n"
+#        f = open(self.logfile,'w')
+#        f.write(ostr)
+#        f.close()
+#        return ok, errs
 
 class Rlog(BuildPkg):
     def _configure(self):
