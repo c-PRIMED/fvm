544f770b8704d6f586d9ae5bd942edf07e0b8002
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-04-10 13:26:28 +0000

    update

--- src/fvm/scripts/testThermalModelDistMesh.py
+++ src/fvm/scripts/testThermalModelDistMesh.py
@@ -6,7 +6,7 @@ sys.setdlopenflags(0x100|0x2)
 import fvmbaseExt
 import importers
 import time
-
+from numpy import *
 atype = 'double'
 #atype = 'tangent'
 
@@ -40,6 +40,118 @@ def advance(fmodel,niter):
         except KeyboardInterrupt:
             break
 
+
+def dumpTecplotFile(nmesh, meshes):
+  #cell sites
+  cellSites = []
+  for n in range(0,nmesh):
+     cellSites.append( meshes[n].getCells() )
+     print "cellSites[", n, "].getCount = ", cellSites[n].getCount()
+
+  #face sites
+  faceSites = []
+  for n in range(0,nmesh):
+     faceSites.append( meshes[n].getFaces() )
+
+  #node sites
+  nodeSites = []
+  for n in range(0,nmesh):
+     nodeSites.append( meshes[n].getNodes() )
+
+  #get connectivity (faceCells)
+  faceCells = []
+  for n in range(0,nmesh):
+     faceCells.append( meshes[n].getConnectivity( faceSites[n], cellSites[n] ) )
+ 
+  #get connectivity ( cellNodes )
+  cellNodes = []
+  for n in range(0,nmesh):
+     cellNodes.append( meshes[n].getCellNodes() )
+
+  #get Volume as array
+  volumes = []
+  for n in range(0,nmesh):
+     volumes.append( geomFields.volume[cellSites[n]].asNumPyArray() )
+ 
+  cellCentroids =[]
+  for n in range(0,nmesh):
+     cellCentroids.append( geomFields.coordinate[cellSites[n]].asNumPyArray() )
+
+  velFields = []
+  for n in range(0,nmesh):
+     velFields.append( thermalFields.temperature[cellSites[n]].asNumPyArray() )
+
+  coords = []
+  for n in range(0,nmesh):
+     coords.append( meshes[n].getNodeCoordinates().asNumPyArray() )
+     print "shape( coords[", n, "] ) = ", shape( coords[n] )	
+     
+
+  f = open("dist_mesh_tecplot.dat", 'w')
+
+  f.write("Title = \" tecplot file for 2D Cavity problem \" \n")
+  f.write("variables = \"x\", \"y\", \"z\", \"velX\", \"cellCentroidY\" \n")
+  for n in range(0,nmesh):
+     title_name = "nmesh" + str(n)
+     ncell  = cellSites[n].getSelfCount()
+     nnode  = nodeSites[n].getCount()
+     zone_name = "Zone T = " + "\"" + title_name +  "\"" +      \
+                 " N = " + str( nodeSites[n].getCount() ) +     \
+		 " E = " + str( ncell ) +  \
+		 " DATAPACKING = BLOCK, VARLOCATION = ([4-5]=CELLCENTERED), " + \
+		 " ZONETYPE=FETRIANGLE \n"
+     f.write( zone_name )
+     #write x
+     for i in range(0,nnode):
+          f.write(str(coords[n][i][0])+"    ")
+	  if ( i % 5 == 4 ):
+	     f.write("\n")
+     f.write("\n")	  
+     
+     #write y
+     for i in range(0,nnode):
+          f.write(str(coords[n][i][1])+"    ")
+	  if ( i % 5 == 4 ):
+	     f.write("\n")
+     f.write("\n")	  
+
+     #write z
+     for i in range(0,nnode):
+          f.write(str(coords[n][i][2])+"    ")
+	  if ( i % 5 == 4 ):
+	     f.write("\n")
+     f.write("\n")	  
+     
+     
+     #write velX
+     for i in range(0,ncell):
+        f.write( str(velFields[n][i]) + "    ")
+	if ( i % 5  == 4 ):
+	    f.write("\n")
+     f.write("\n")
+
+     #write velX
+     for i in range(0,ncell):
+        f.write( str(cellCentroids[n][i][1]) + "    ")
+	if ( i % 5  == 4 ):
+	    f.write("\n")
+     f.write("\n")
+     	  	    
+   
+     #connectivity
+     for i in range(0,ncell):
+        nnodes_per_cell = cellNodes[n].getCount(i)
+        for node in range(0,nnodes_per_cell):
+	    f.write( str(cellNodes[n](i,node)+1) + "     ")
+        f.write("\n")
+
+     	    
+	    
+     f.write("\n")	  
+     
+	
+  
+  f.close()
 # change as needed
 #import debug
 
@@ -82,7 +194,7 @@ tmodel = models.ThermalModelA(geomFields,thermalFields,meshes)
 ## set bc for top to be at 400
 bc3 = tmodel.getBCMap()[3]
 bc3.bcType = 'SpecifiedTemperature'
-bc3.setVar('specifiedTemperature',400)
+bc3.setVar('specifiedTemperature',300)
 
 bc4 = tmodel.getBCMap()[4]
 bc4.bcType = 'SpecifiedTemperature'
@@ -108,3 +220,5 @@ toptions.linearSolver = tSolver
 #import debug
 tmodel.init()
 tmodel.advance(1)
+
+dumpTecplotFile(nmesh, meshes)
