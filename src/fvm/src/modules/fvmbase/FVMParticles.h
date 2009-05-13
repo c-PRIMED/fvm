628ef9775100a92be1a28f8300896873d4d289fc
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-05-13 18:34:30 +0000

    FVM particles identification class (identify FVM particles (fluid) around IBM cells),

---
+++ src/fvm/src/modules/fvmbase/FVMParticles.h
@@ -0,0 +1,47 @@
+//
+// C++ Interface: fvmparticles
+//
+// Description: 
+//
+//
+// Author: yildirim,,, <yildirim@prism>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef FVMPARTICLES_H
+#define FVMPARTICLES_H
+
+#include "Mesh.h"
+#include "Array.h"
+#include <vector>
+#include <set>
+
+/**
+	@author yildirim,,, <yildirim@prism>
+*/
+class FVMParticles{
+
+    public:
+       typedef   shared_ptr< Array<int> >     ArrayIntPtr;
+
+       FVMParticles( const MeshList& meshList );
+      ~FVMParticles();
+
+       void   setParticles( int sweep );
+       void   setSweepIter( int sweep) { _sweepIter = sweep; };
+
+       const ArrayBase&  getCellIDs( int mesh_id ) const { return *_cellID.at(mesh_id); }
+
+
+    private:
+
+       const MeshList _meshList;
+       int   _sweepIter;
+
+       int _nmesh;
+       vector< set<int> >  _cellIDSet;
+       vector< ArrayIntPtr >  _cellID;
+};
+
+#endif
