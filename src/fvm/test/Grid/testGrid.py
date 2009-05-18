0446da78c72b1c2360d941018d4fab04bb8c8787
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-05-18 16:02:53 +0000

    get fvm Grid test working

--- src/fvm/test/Grid/testGrid.py
+++ src/fvm/test/Grid/testGrid.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
-import pdb
 import sys
 sys.setdlopenflags(0x100|0x2)
-
-import fvmbaseExt
-import importers
-
+import os, fvmbaseExt, importers
+from mpi4py import MPI
 
 atype = 'double'
 #atype = 'tangent'
-
 if atype == 'double':
     import models_atyped_double as models
     import exporters_atyped_double as exporters
@@ -17,51 +13,28 @@ elif atype == 'tangent':
     import models_atyped_tangent_double as models
     import exporters_atyped_tangent_double as exporters
 
-
 from FluentCase import FluentCase
 
+fileBase = os.getcwd()
+coordFile = os.path.join(fileBase, "Grid_Coord.dat")
+velocityFile = os.path.join(fileBase, "Grid_Velocity.dat")
+infile = os.path.join(fileBase, "3D-cantilever.cas")
+outfile = '/dev/stdout'
 
-fileBase = "/home/lin/work/app-memosa/src/fvm/test/Grid/"
-coordFile = fileBase + "Grid_Coord.dat"
-velocityFile = fileBase + "Grid_Velocity.dat"
-
-def usage():
-    print "Usage: %s filebase [outfilename]" % sys.argv[0]
-    print "Where filebase.cas is a Fluent case file."
-    print "Output will be in filebase-cellmark.dat if it is not specified."
-    sys.exit(1)
-
-outfile = None
-if __name__ == '__main__' and fileBase is None:
-    if len(sys.argv) < 2:
-        usage()
-    fileBase = sys.argv[1]
-    if len(sys.argv) == 3:
-        outfile = sys.argv[2]
-
-if outfile == None:
-    outfile = fileBase+"-cellmark.dat"
-    
-reader = FluentCase(fileBase+"3D-cantilever.cas")
-
-#import ddd
+reader = FluentCase(infile)
 reader.read();
 
 meshes = reader.getMeshList()
-
 mesh0 = meshes[0]
-
 geomFields =  models.GeomFields('geom')
 
 metricsCalculator = models.MeshMetricsCalculatorA(geomFields,meshes)
-
 metricsCalculator.init()
 
 if atype == 'tangent':
     metricsCalculator.setTangentCoords(0,7,1)
 
 flowFields =  models.FlowFields('flow')
-
 fmodel = models.FlowModelA(geomFields,flowFields,meshes)
 
 reader.importFlowBCs(fmodel)
@@ -73,14 +46,13 @@ t0 = time.time()
 grid = fvmbaseExt.Grid(geomFields, flowFields, coordFile, velocityFile)
 gridNodes = grid.getNodes()
 
-
 sideID = 10
 topID = 9
 botID = 12
 tipID = 11
 
-faceVelFile = open(fileBase + "InterpolatedFaceVelocity.dat", "w")
-#import ddd
+
+faceVelFile = open(outfile, "w")
 for mesh in meshes:
     fgs = mesh.getBoundaryGroups()
     for fg in fgs:
@@ -108,7 +80,5 @@ faceVelFile.close()
 t1 = time.time()
 if outfile != '/dev/stdout':
     print '\nsolution time = %f' % (t1-t0)
-
-print 'solution time = %f' % (t1-t0)
 
 
