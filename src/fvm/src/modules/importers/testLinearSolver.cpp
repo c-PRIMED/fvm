b063a83d34ea91e821808b61ce9cf5f4489fffeb
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-05-18 17:27:23 +0000

    make testlinearsolver work with MPI

--- src/fvm/src/modules/importers/testLinearSolver.cpp
+++ src/fvm/src/modules/importers/testLinearSolver.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 #include "MMReader.h"
 #include "AMG.h"
+#ifdef FVM_PARALLEL
+#include <mpi.h>
+#endif
 
 int main(int argc, char *argv[])
 {
+#ifdef FVM_PARALLEL
+  MPI::Init(argc, argv);
+#endif
   MMReader reader(argv[1], argv[2]);
   
   shared_ptr<LinearSystem> ls(reader.getLS());
