a5f91dc18c2c7c7c8146edd9065a8d5ea4edb39d
srmathur <srmathur@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-06-30 17:40:08 +0000

    updated to use modified implementation of slip jump

--- src/fvm/scripts/testIG_Slip.py
+++ src/fvm/scripts/testIG_Slip.py
@@ -22,10 +22,12 @@ from FluentCase import FluentCase
 #fvmbaseExt.enableDebug("cdtor")
 
 fileBase = None
-numIterations = 2000
-#fileBase = "/home/sm/prism-meshes/slipbc/Noslipbc_ig"
+numIterations = 5000
+fileBase = "/home/shared/app-memosa/src/fvm/test/testIG_Slip"
+#fileBase = "/home/shared/app-memosa/src/fvm/test/fine2"
+
 #fileBase = "/home/sm/a/data/wj"
-fileBase="/trunk/src/fvm/test/testIG_Slip"
+#fileBase="/trunk/src/fvm/test/testIG_Slip"
 
 def usage():
     print "Usage: %s filebase [outfilename]" % sys.argv[0]
@@ -40,7 +42,7 @@ def advance(models,dmodel,nstart,niter):
             for m in models:
                 if not m.advance(1):
                     converged = False
-            if (i%5) == 0:
+            if (i%10) == 0:
                 dmodel.advance(1)
             if converged:
                 break
@@ -65,8 +67,8 @@ def advancenew(models,dmodel,nstart,niter):
             for m in models:
                 m.advance(1)
                     
-            if (i%5)== 0:
-                dmodel.advance(1)
+            #if (i%25)== 0:
+            #    dmodel.advance(1)
             
         except KeyboardInterrupt:
             break
@@ -88,6 +90,7 @@ if outfile == None:
 reader = FluentCase(fileBase+".cas")
 
 #import debug
+#import ddd
 reader.read();
 
 meshes = reader.getMeshList()
@@ -107,15 +110,6 @@ flowFields =  models.FlowFields('flow')
 
 fmodel = models.FlowModelA(geomFields,flowFields,meshes)
 
-dmodel = models.IdealGasDensityModelA(geomFields,flowFields,meshes)
-
-## set density  model settings
-for i,vc in dmodel.getVCMap().iteritems():
-    vc['pressure'] = flowFields.pressure
-    vc['temperature']= 300.0
-    vc['operatingPressure'] = 101325.0
-    vc['molecularWeight']= 28.9645
-
 
 reader.importFlowBCs(fmodel)
 
@@ -124,7 +118,7 @@ reader.importFlowBCs(fmodel)
 for i,bc in fmodel.getBCMap().iteritems():
     if bc.bcType == 'NoSlipWall':
         bc.bcType = 'SlipJump'
-        bc.accomodationCoefficient = 1.0
+        bc.setVar('accomodationCoefficient', 1.0)
 
 #calculation of KnudsenNumber*H=meanfreepath
 T_N2=300.0
@@ -133,13 +127,12 @@ P_N2=101325.0 #operating pressure
 R_N2=287.0   #gas constant
 pi=3.1416
 lambda_N2=mu_N2/P_N2*(pi*R_N2*T_N2*0.5)**0.5
-fmodel.KnudsenNumber = lambda_N2
 #fmodel.KnudsenNumber = 0.1*1e-6
     
 print "knudsen %s" %(lambda_N2/1e-6)
 
 momSolver = fvmbaseExt.AMG()
-momSolver.relativeTolerance = 1e-1
+momSolver.relativeTolerance = 1e-3
 momSolver.nMaxIterations = 20
 momSolver.maxCoarseLevels=20
 momSolver.verbosity=0
@@ -149,7 +142,7 @@ contSolver = fvmbaseExt.AMG()
 #pc.verbosity=0
 #contSolver = fvmbaseExt.BCGStab()
 #contSolver.preconditioner = pc
-contSolver.relativeTolerance = 1e-1
+contSolver.relativeTolerance = 1e-3
 contSolver.nMaxIterations = 20
 contSolver.verbosity=0
 contSolver.maxCoarseLevels=20
@@ -161,9 +154,29 @@ foptions.pressureLinearSolver = contSolver
 
 foptions.momentumTolerance=1e-5
 foptions.continuityTolerance=1e-5
+foptions.printNormalizedResiduals=False
+
 foptions.setVar("momentumURF",0.7)
 foptions.setVar("pressureURF",0.3)
-foptions.printNormalizedResiduals=False
+foptions.setVar("initialXVelocity",6)
+
+foptions.incompressible = False
+foptions.setVar('operatingPressure', 101325.0)
+foptions.setVar('operatingTemperature', 300.0)
+foptions.setVar('molecularWeight',28.9645)
+
+dmodel = models.IdealGasDensityModelA(geomFields,flowFields,meshes)
+
+## set density  model settings
+for i,vc in dmodel.getVCMap().iteritems():
+
+    ## use local pressure + op pressure 
+    vc['pressure'] = flowFields.pressure
+    # use constant temperature
+    vc['temperature']= foptions.getVar('operatingTemperature')
+    vc['operatingPressure'] = foptions.getVar('operatingPressure')
+    vc['molecularWeight']= foptions.getVar('molecularWeight')
+    vc['urf']= 0.5
 
 """
 if atype=='tangent':
@@ -175,9 +188,10 @@ if atype=='tangent':
 #import debug
 
 fmodel.init()
+dmodel.init()
 #fmodel.advance(numIterations)
-initIterations=5
-advancenew([fmodel],dmodel,0,initIterations)
+initIterations=0
+#advancenew([fmodel],dmodel,0,initIterations)
 advance([fmodel],dmodel,initIterations,numIterations)
 #advance([fmodel],dmodel,numIterations)-old
 
