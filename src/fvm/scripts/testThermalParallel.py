4518921b035a688b3633a8c9bac516f580c14dea
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-09-30 18:46:42 +0000

    updated script for temp. solver

--- src/fvm/scripts/testThermalParallel.py
+++ src/fvm/scripts/testThermalParallel.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
+"""
+Usage: testThermalParallel.py [options] infile
+options are:
+--type  'tri'[default], 'quad', 'hexa', or 'tetra'
+--xdmf  Dump data in xdmf
+"""
 
 import sys
 sys.setdlopenflags(0x100|0x2)
-
-import fvmbaseExt
-import importers
-import fvmparallel
-import time
+import fvmbaseExt, importers, fvmparallel, time
 from numpy import *
 from mpi4py  import MPI
+from optparse import OptionParser
+
 atype = 'double'
 #atype = 'tangent'
 
@@ -19,51 +23,49 @@ elif atype == 'tangent':
     import models_atyped_tangent_double as models
     import exporters_atyped_tangent_double as exporters
 
-
 from FluentCase import FluentCase
 
-#fvmbaseExt.enableDebug("cdtor")
-
-fileBase = None
-numIterations = 10
-#fileBase = "/home/yildirim/memosa/src/fvm/test/cav_44_tri"
-fileBase = "/home/yildirim/memosa/src/fvm/test/tri_894"
-#fileBase = "/home/yildirim/memosa/src/fvm/test/cav_tri_915K"
-#fileBase = "/home/yildirim/memosa/src/fvm/test/test_tri"
-
 def usage():
-    print "Usage: %s filebase [outfilename]" % sys.argv[0]
-    print "Where filebase.cas is a Fluent case file."
-    print "Output will be in filebase-prism.dat if it is not specified."
+    print __doc__
     sys.exit(1)
 
-def advance(fmodel,niter):
-    for i in range(0,niter):
-        try:
-            fmodel.advance(1)
-        except KeyboardInterrupt:
-            break
-
+# map between fvm, tecplot, and xdmf types
+etype = {
+        'tri' : 1,
+        'quad' : 2,
+        'tetra' : 3,
+        'hexa' : 4
+        }
+tectype = {
+        'tri' : 'FETRIANGLE',
+        'quad' : 'FEQUADRILATERAL',
+        'tetra' : 'FETETRAHEDRON',
+        'hexa' : 'FEBRICK'
+        }
+xtype = {
+        'tri' : 'Triangle',
+        'quad' : 'Quadrilateral',
+        'tetra' : 'Tetrahedron',
+        'hexa' : 'Hexahedron'
+        }
 
 def dumpMPITimeProfile(part_mesh_maxtime, part_mesh_mintime, solver_maxtime, solver_mintime):
+    fname = "time_mpi_totalprocs%s.dat" % MPI.COMM_WORLD.Get_size()
+    f = open(fname,'w')
+    line = " part_mesh_mintime = " + str(part_mesh_mintime[0]) + "\n" + \
+        " part_mesh_maxtime = " + str(part_mesh_maxtime[0]) + "\n" + \
+        " solver_mintime    = " + str(solver_mintime[0])    + "\n" + \
+        " solver_maxtime    = " + str(solver_maxtime[0])    + "\n"
+    print line
+    f.write(line)
+    f.close()
 
-      fname = "time_mpi_totalprocs"  + str(MPI.COMM_WORLD.Get_size()) + ".dat"
-      f = open(fname,'w')
-      line = " part_mesh_mintime = " + str(part_mesh_mintime[0]) + "\n" + \
-             " part_mesh_maxtime = " + str(part_mesh_maxtime[0]) + "\n" + \
-             " solver_mintime    = " + str(solver_mintime[0])    + "\n" + \
-             " solver_maxtime    = " + str(solver_maxtime[0])    + "\n"
-      print line
-      f.write(line)
-      f.close()
-
-
-def dumpTecplotFile(nmesh, meshes):
+def dumpTecplotFile(nmesh, meshes, mtype):
   #cell sites
   cellSites = []
   for n in range(0,nmesh):
      cellSites.append( meshes[n].getCells() )
-     print "cellSites[", n, "].getCount = ", cellSites[n].getCount()
+#     print "cellSites[", n, "].getCount = ", cellSites[n].getCount()
 
   #face sites
   faceSites = []
@@ -101,23 +103,17 @@ def dumpTecplotFile(nmesh, meshes):
   coords = []
   for n in range(0,nmesh):
      coords.append( meshes[n].getNodeCoordinates().asNumPyArray() )
-     print "shape( coords[", n, "] ) = ", shape( coords[n] )	
+#     print "shape( coords[", n, "] ) = ", shape( coords[n] )	
      
-  file_name = "temp_proc" + str(MPI.COMM_WORLD.Get_rank()) + ".dat"
-  f = open(file_name, 'w')
-
+  f = open("temp_proc%s.dat" % MPI.COMM_WORLD.Get_rank(), 'w')
   f.write("Title = \" tecplot file for 2D Cavity problem \" \n")
   f.write("variables = \"x\", \"y\", \"z\", \"velX\", \"cellCentroidY\" \n")
   for n in range(0,nmesh):
-     title_name = "nmesh" + str(n)
+     title_name = "nmesh%s" % n
      ncell  = cellSites[n].getSelfCount()
      nnode  = nodeSites[n].getCount()
-     zone_name = "Zone T = " + "\"" + title_name +  "\"" +      \
-                 " N = " + str( nodeSites[n].getCount() ) +     \
-		 " E = " + str( ncell ) +  \
-		 " DATAPACKING = BLOCK, VARLOCATION = ([4-5]=CELLCENTERED), " + \
-		 " ZONETYPE=FETRIANGLE \n"
-     f.write( zone_name )
+     f.write("Zone T = \"%s\" N = %s E = %s DATAPACKING = BLOCK, VARLOCATION = ([4-5]=CELLCENTERED), ZONETYPE=%s\n" %
+             (title_name,  nodeSites[n].getCount(), ncell, tectype[mtype]))
      #write x
      for i in range(0,nnode):
           f.write(str(coords[n][i][0])+"    ")
@@ -139,7 +135,6 @@ def dumpTecplotFile(nmesh, meshes):
 	     f.write("\n")
      f.write("\n")	  
      
-     
      #write velX
      for i in range(0,ncell):
         f.write( str(velFields[n][i]) + "    ")
@@ -154,93 +149,193 @@ def dumpTecplotFile(nmesh, meshes):
 	    f.write("\n")
      f.write("\n")
      	  	    
-   
      #connectivity
      for i in range(0,ncell):
         nnodes_per_cell = cellNodes[n].getCount(i)
         for node in range(0,nnodes_per_cell):
 	    f.write( str(cellNodes[n](i,node)+1) + "     ")
         f.write("\n")
-
-     	    
-	    
      f.write("\n")	  
-     
-	
-  
   f.close()
-# change as needed
-#import debug
 
-reader = FluentCase(fileBase+".cas")
+def writeXdmfHeader():
+    nprocs = MPI.COMM_WORLD.Get_size()
+    mesh_file = open("mesh.xmf", "w")
+    mesh_file.write("<?xml version='1.0' ?>\n")
+    mesh_file.write("<!DOCTYPE Xdmf SYSTEM 'Xdmf.dtd' []>\n")
+    mesh_file.write("<Xdmf xmlns:xi='http://www.w3.org/2001/XInclude' Version='2.0'>\n")
+    mesh_file.write("  <Domain>\n")
+    for i in range(0,nprocs):
+        mesh_file.write("    <xi:include href='mesh_proc%s.xmf' />\n" % i)
+    mesh_file.write("  </Domain>\n")
+    mesh_file.write("</Xdmf>\n")
+    mesh_file.close()
+    
+def dumpXdmfFile(nmesh, meshes, mtype):
+    proc = MPI.COMM_WORLD.Get_rank()
+    if proc == 0:
+        writeXdmfHeader()
+
+    # cell sites
+    cellSites = []
+    for n in range(0,nmesh):
+        cellSites.append( meshes[n].getCells() )
+
+    # face sites
+    faceSites = []
+    for n in range(0,nmesh):
+        faceSites.append( meshes[n].getFaces() )
+
+    # node sites
+    nodeSites = []
+    for n in range(0,nmesh):
+        nodeSites.append( meshes[n].getNodes() )
+
+    # get connectivity (faceCells)
+    faceCells = []
+    for n in range(0,nmesh):
+        faceCells.append( meshes[n].getConnectivity( faceSites[n], cellSites[n] ) )
+ 
+    # get connectivity ( cellNodes )
+    cellNodes = []
+    for n in range(0,nmesh):
+        cellNodes.append( meshes[n].getCellNodes() )
+
+    # get Volume as array
+    volumes = []
+    for n in range(0,nmesh):
+        volumes.append( geomFields.volume[cellSites[n]].asNumPyArray() )
+ 
+    cellCentroids =[]
+    for n in range(0,nmesh):
+        cellCentroids.append( geomFields.coordinate[cellSites[n]].asNumPyArray() )
+
+    velFields = []
+    for n in range(0,nmesh):
+        velFields.append( thermalFields.temperature[cellSites[n]].asNumPyArray() )
+
+    coords = []
+    for n in range(0,nmesh):
+        coords.append( meshes[n].getNodeCoordinates().asNumPyArray() )
+#        print "shape( coords[", n, "] ) = ", shape( coords[n] )	
+     
+    f = open("mesh_proc%s.xmf" % proc, 'w')
+    for n in range(0,nmesh):
+        f.write("<Grid Name='Mesh%s-%s' GridType='Uniform'>\n" % (proc, n))
+        ncell  = cellSites[n].getSelfCount()
+        nnode  = nodeSites[n].getCount()
+        f.write("  <Topology TopologyType='%s' Dimensions='%s'>\n" % (xtype[mtype], ncell))
+        f.write("    <DataItem Dimensions='%s %s'>\n" % (ncell, cellNodes[0].getCount(0)))
+
+        # connectivity (topology)
+        for i in range(0,ncell):
+            f.write("      ")
+            nnodes_per_cell = cellNodes[n].getCount(i)
+            for node in range(0, nnodes_per_cell):
+                f.write("%s " % cellNodes[n](i,node))
+            f.write("\n")
+        f.write("    </DataItem>\n")
+        f.write("  </Topology>\n")
+
+        # Geometry
+        f.write("  <Geometry Type='XYZ'>\n")
+        f.write("    <DataItem Dimensions='%s 3' NumberType='Float'>\n" % nnode)
+        for i in range(0,nnode):
+            f.write("      %s %s %s\n" % (coords[n][i][0], coords[n][i][1], coords[n][i][2]))
+        f.write("    </DataItem>\n")     
+        f.write("  </Geometry>\n")
+
+        # ATTRIBUTES
+
+        # velX
+        f.write("  <Attribute Name='velX' Center='Cell'>\n")
+        f.write("    <DataItem Dimensions='%s'>" % ncell)
+        for i in range(0,ncell):
+            if ( i % 5  == 0 ):
+                f.write("\n      ")
+            f.write("%s " % velFields[n][i])
+        f.write("\n")
+        f.write("    </DataItem>\n")     
+        f.write("  </Attribute>\n")
+
+        # cellCentroids
+        f.write("  <Attribute Name='cellCentroids' Center='Cell'>\n")
+        f.write("    <DataItem Dimensions='%s'>" % ncell)
+        for i in range(0,ncell):
+            if ( i % 5  == 0 ):
+                f.write("\n      ")
+            f.write("%s " % cellCentroids[n][i][1])
+        f.write("\n")
+        f.write("    </DataItem>\n")     
+        f.write("  </Attribute>\n")
+
+        f.write("</Grid>\n")
+    f.close()
+
+
+parser = OptionParser()
+parser.set_defaults(type='tri')
+parser.add_option("--type", help="'tri'[default], 'quad', 'hexa', or 'tetra'")
+parser.add_option("--xdmf", action='store_true', help="Dump data in xdmf")
+parser.add_option("--time","-t",action='store_true',help="Print timing information.")
+(options, args) = parser.parse_args()
+if len(args) != 1:
+    usage()
+
+numIterations = 10
+reader = FluentCase(args[0])
 reader.read()
 fluent_meshes = reader.getMeshList()
 
 nmesh = 1
-
-import time
-t0 = time.time()
-
-
-
-#print "nmesh = ", nmesh
-#npart = fvmparallel.IntVector(1,nmesh)  #total of distributed meshes
-#etype = fvmparallel.IntVector(1,1) #triangle
-
 npart = [MPI.COMM_WORLD.Get_size()]
-etype = [1]
+etype = [etype[options.type]]
+
+if not MPI.COMM_WORLD.Get_rank():
+   print "parmesh is processing"
+     
+    
+
+if options.time:
+    # time profile for partmesh
+    part_mesh_time = zeros(1,dtype='d')
+    part_mesh_start = zeros(1, dtype='d')
+    part_mesh_end   = zeros(1, dtype='d')
+    part_mesh_maxtime = zeros(1,dtype='d')
+    part_mesh_mintime = zeros(1, dtype='d')
+    part_mesh_start[0] = MPI.Wtime()
 
 #partMesh constructor and setTypes
-
-#time profile for partmesh
-part_mesh_time = zeros(1,dtype='d')
-part_mesh_start = zeros(1, dtype='d')
-part_mesh_end   = zeros(1, dtype='d')
-part_mesh_maxtime = zeros(1,dtype='d')
-part_mesh_mintime = zeros(1, dtype='d')
-
-part_mesh_start[0] = MPI.Wtime()
-
 part_mesh = fvmparallel.PartMesh( fluent_meshes, npart, etype );
 part_mesh.setWeightType(0);
 part_mesh.setNumFlag(0);
 
-#print "nmesh = ", nmesh,  "procID = ", MPI.COMM_WORLD.Get_rank() 
 #actions
 part_mesh.partition()
 part_mesh.mesh()
-part_mesh.mesh_debug()
+#part_mesh.mesh_debug()
 meshes = part_mesh.meshList()
-
-part_mesh_end[0] = MPI.Wtime()
-part_mesh_time[0] = part_mesh_end[0] - part_mesh_start[0]
-MPI.COMM_WORLD.Allreduce( [part_mesh_time,MPI.DOUBLE], [part_mesh_maxtime, MPI.DOUBLE], op=MPI.MAX) 
-MPI.COMM_WORLD.Allreduce( [part_mesh_time,MPI.DOUBLE], [part_mesh_mintime, MPI.DOUBLE], op=MPI.MIN) 
-
-
-#print "procid = ", MPI.COMM_WORLD.Get_rank(), " part_mesh_time = ", \
-#                   part_mesh_time, "part_mesh_mintime = ",  part_mesh_mintime, \
-#                   part_mesh_time, "part_mesh_maxtime = ",  part_mesh_maxtime 
-
-solver_start   = zeros(1, dtype='d')
-solver_end     = zeros(1, dtype='d')
-solver_time    = zeros(1, dtype='d')
-solver_maxtime = zeros(1,dtype='d')
-solver_mintime = zeros(1,dtype='d')
-
-solver_start[0] = MPI.Wtime()
+if options.time:
+    part_mesh_end[0] = MPI.Wtime()
+    part_mesh_time[0] = part_mesh_end[0] - part_mesh_start[0]
+    MPI.COMM_WORLD.Allreduce( [part_mesh_time,MPI.DOUBLE], [part_mesh_maxtime, MPI.DOUBLE], op=MPI.MAX) 
+    MPI.COMM_WORLD.Allreduce( [part_mesh_time,MPI.DOUBLE], [part_mesh_mintime, MPI.DOUBLE], op=MPI.MIN) 
+    solver_start   = zeros(1, dtype='d')
+    solver_end     = zeros(1, dtype='d')
+    solver_time    = zeros(1, dtype='d')
+    solver_maxtime = zeros(1,dtype='d')
+    solver_mintime = zeros(1,dtype='d')
+    solver_start[0] = MPI.Wtime()
 
 geomFields =  models.GeomFields('geom')
 
 metricsCalculator = models.MeshMetricsCalculatorA(geomFields,meshes)
-
 metricsCalculator.init()
 
 if atype == 'tangent':
     metricsCalculator.setTangentCoords(0,7,1)
 
 thermalFields =  models.ThermalFields('therm')
-
 tmodel = models.ThermalModelA(geomFields,thermalFields,meshes)
 
 
@@ -255,6 +350,14 @@ if 4 in bcMap:
    bc4 = tmodel.getBCMap()[4]
    bc4.bcType = 'SpecifiedTemperature'
    bc4.setVar('specifiedTemperature',0)
+if 5 in bcMap:
+   bc5 = tmodel.getBCMap()[5]
+   bc5.bcType = 'SpecifiedTemperature'
+   bc5.setVar('specifiedTemperature',0)
+if 6 in bcMap:
+   bc6 = tmodel.getBCMap()[6]
+   bc6.bcType = 'SpecifiedTemperature'
+   bc6.setVar('specifiedTemperature',0)
 
 ## set viscosity and density, this is done per mesh since each mesh has its own VC object
 #vcMap = tmodel.getVCMap()
@@ -262,30 +365,42 @@ if 4 in bcMap:
 #    vc.setVar('density',1.0)
 #    vc.setVar('thermalConductivity',1.0)
 
+#########################
+pc = fvmbaseExt.AMG()
+#pc.setMergeLEvelSize(40000)
+pc.verbosity=3
 
-tSolver = fvmbaseExt.AMG()
-tSolver.relativeTolerance = 1e-5
-tSolver.nMaxIterations = 2000
-tSolver.maxCoarseLevels=20
-tSolver.verbosity=2
+tSolver = fvmbaseExt.BCGStab()
+tSolver.preconditioner=pc
+tSolver.relativeTolerance = 1e-1
+tSolver.nMaxIterations = 200000
+tSolver.verbosity=3
+
+
+#####################
+#tSolver = fvmbaseExt.AMG()
+#tSolver.relativeTolerance = 1e-4
+#tSolver.nMaxIterations = 200000
+#tSolver.maxCoarseLevels=20
+#tSolver.verbosity=3
+#tSolver.setMergeLevelSize(40000)
 
 toptions = tmodel.getOptions()
-
 toptions.linearSolver = tSolver
 
 #import debug
 tmodel.init()
-print "nmesh = ", nmesh,  "procID = ", MPI.COMM_WORLD.Get_rank() 
+#print "nmesh = ", nmesh,  "procID = ", MPI.COMM_WORLD.Get_rank() 
 tmodel.advance(1)
 
-solver_end[0] = MPI.Wtime()
-solver_time[0] = solver_end[0] - solver_start[0]
-MPI.COMM_WORLD.Allreduce( [solver_time,MPI.DOUBLE], [solver_maxtime, MPI.DOUBLE], op=MPI.MAX) 
-MPI.COMM_WORLD.Allreduce( [solver_time,MPI.DOUBLE], [solver_mintime, MPI.DOUBLE], op=MPI.MIN) 
+if options.time:
+    solver_end[0] = MPI.Wtime()
+    solver_time[0] = solver_end[0] - solver_start[0]
+    MPI.COMM_WORLD.Allreduce( [solver_time,MPI.DOUBLE], [solver_maxtime, MPI.DOUBLE], op=MPI.MAX) 
+    MPI.COMM_WORLD.Allreduce( [solver_time,MPI.DOUBLE], [solver_mintime, MPI.DOUBLE], op=MPI.MIN) 
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        dumpMPITimeProfile(part_mesh_maxtime, part_mesh_mintime, solver_maxtime, solver_mintime)
 
-if MPI.COMM_WORLD.Get_rank() == 0:
-   dumpMPITimeProfile(part_mesh_maxtime, part_mesh_mintime, solver_maxtime, solver_mintime)
-
-
-
-dumpTecplotFile( nmesh, meshes)
+#dumpTecplotFile( nmesh, meshes, options.type)
+#if options.xdmf:
+#    dumpXdmfFile( nmesh, meshes, options.type)
