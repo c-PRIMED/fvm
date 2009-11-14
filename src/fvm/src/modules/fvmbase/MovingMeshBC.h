43915b31443998a3e7b547cc94ac4e7b80b31f0a
shankhadeep <shankhadeep@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-11-14 12:29:22 +0000

    MovingMeshModel Options

---
+++ src/fvm/src/modules/fvmbase/MovingMeshBC.h
@@ -0,0 +1,25 @@
+#include "misc.h"
+#include "FloatVarDict.h"
+
+
+template<class T>
+struct MovingMeshModelOptions : public FloatVarDict<T>
+{
+  MovingMeshModelOptions()
+  {
+      this->defineVar("timeStep",T(0.1));
+
+      this->absTolerance = 1e-4;
+      this->nNodeDisplacementSweeps = 20;
+      this->relativeTolerance = 1e-1;
+      this->timeDiscretizationOrder = 1;
+  }
+  double absTolerance;
+  double relativeTolerance;
+  int nNodeDisplacementSweeps;
+  int timeDiscretizationOrder;
+
+#ifndef SWIG
+#endif
+};
+
