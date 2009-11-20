ba806d40913db68175e691933d90f0e7d2b97c05
srutichigs <srutichigs@8639bca7-a847-0410-9c84-802de9f82a5b>
2009-11-20 17:55:12 +0000

    error with scons resolved

--- src/fvm/src/modules/esbgkbase/quadrature.h
+++ src/fvm/src/modules/esbgkbase/quadrature.h
@@ -5,19 +5,23 @@
 #include "Array.h"
 #include "Vector.h"
 
-class quadrature
+template<class T>
+class quadrature 
 {
  public:
-  typedef Array<double> dArray;
-  typedef Vector<double,3> Vectord3;
-  //typedef Array<Vector<double,3>> Vectord3Array;
-  dArray* absci1Ptr;
+  typedef Array<T> dArray;
+  typedef Vector<T,3> Vectord3;
+  //typedef Array<Vectord3> Vectord3Array;
+  dArray* absci1Ptr; //, absci2Ptr, absci3Ptr;
   dArray* absci2Ptr;
   dArray* absci3Ptr;
-  dArray* wts1Ptr;
+  dArray* wts1Ptr; //, wts2Ptr,wts3Ptr;
   dArray* wts2Ptr; 
   dArray* wts3Ptr;
-  
+  dArray* cxPtr;
+  dArray* cyPtr;
+  dArray* czPtr;
+  dArray* dcxyzPtr;
   // quadrature(int,int)
   quadrature(int N1, int N2, int N3, double T2)
     {
@@ -31,13 +35,17 @@ class quadrature
       dArray & wts1= *wts1Ptr;
       wts2Ptr=new dArray(N2);
       dArray & wts2= *wts2Ptr; 
-       wts3Ptr=new dArray(N3);
+      wts3Ptr=new dArray(N3);
       dArray & wts3= *wts3Ptr;
-     
+      int N123=N1*N2*N3;
+      cxPtr= new dArray(N123); cyPtr= new dArray(N123); czPtr= new dArray(N123);
+      dcxyzPtr= new dArray(N123);
+      dArray & cx= *cxPtr;dArray & cy= *cyPtr;dArray & cz= *czPtr;
+      dArray & dcxyz= *dcxyzPtr;
       //Array<Vector<double,3>> cxyz;
-      double cxmin,cxmax,cymin,cymax,czmin,czmax;
-      double dcx,dcy,dcz;
-      double clim = 5.5;
+      T cxmin,cxmax,cymin,cymax,czmin,czmax;
+      T dcx,dcy,dcz;
+      const T clim (5.5);
       cxmin=-clim*sqrt(0.5*T2);cxmax=clim*sqrt(0.5*T2);
       cymin=-clim*sqrt(0.5*T2);cymax=clim*sqrt(0.5*T2);
       czmin=-clim*sqrt(0.5*T2);czmax=clim*sqrt(0.5*T2);
@@ -57,6 +65,221 @@ class quadrature
 	absci1[j1]=absci1[j1-1]+dcx;	
 	wts1[j1]=dcx;
       }
+      
+      int j;
+      j=0;
+      for(int j3=0;j3<N3;j3++){
+	for (int j2=0;j2<N2;j2++){
+	  for (int j1=0;j1<N1;j1++){
+	    cx[j]=absci1[j1];	
+	    cy[j]=absci2[j2];
+	    cz[j]=absci3[j3];
+	    dcxyz[j]=wts1[j1]*wts2[j2]*wts3[j3];
+	    j++;
+	  }
+	}
+      }
+      
+    }
+  quadrature(int option_ur, int Nr, int option_theta, int n_int,int option_phi,int nphi_int)
+    // add a comment explaining options in Gauss-Hermite quadrature
+    {
+      int N1,N2,N3;
+      if(option_ur ==0){
+	N1=Nr;}
+      else{N1=option_ur;}
+      if(option_theta ==0){
+	N2=n_int;}
+      else{N2=4*n_int;}
+      if(option_phi ==0){
+	N3=n_int;}
+      else{N3=4*n_int+1;}
+      absci1Ptr=new dArray(N1);
+      dArray & absci1= *absci1Ptr;
+      absci2Ptr=new dArray(N2);
+      dArray & absci2= *absci2Ptr;
+      absci3Ptr=new dArray(N3);
+      dArray & absci3= *absci3Ptr; 
+      wts1Ptr=new dArray(N1);
+      dArray & wts1= *wts1Ptr;
+      wts2Ptr=new dArray(N2);
+      dArray & wts2= *wts2Ptr; 
+      wts3Ptr=new dArray(N3);
+      dArray & wts3= *wts3Ptr;
+
+      int N123=N1*N2*N3;
+      cxPtr= new dArray(N123); cyPtr= new dArray(N123); czPtr= new dArray(N123);
+      dcxyzPtr= new dArray(N123);
+      dArray & cx= *cxPtr;dArray & cy= *cyPtr;dArray & cz= *czPtr;
+      dArray & dcxyz= *dcxyzPtr;
+      switch(option_ur){
+	int j1;
+      case 0:   // constant difference for Ur
+	{double dh1=sqrt(3.0)*3.889/(N1); //neglect ur=.0; 
+	  for (j1=0;j1<N1;j1++){
+	    absci1[j1]=(j1+1)*dh1;
+	    wts1[j1]=dh1*pow(absci1[j1],2.0);
+	  }
+	}break; 
+      case 2:{
+	absci1[0]=0.7539869079898871;
+	absci1[1]=0.1734055298879163E+1;
+	wts1[0]=0.2738413467266824; 
+	wts1[1]=0.1692721159996965;
+	for (j1=0;j1<N1;j1++){
+	  wts1[j1]=wts1[j1]*exp(pow(absci1[j1],2.0));
+	}
+      }break;
+      case 4:            //4-gaussian for y^2*exp(-y^2)
+	{ absci1[0]=0.4238628193900528;
+	  absci1[1]=0.1014332104566760E+1;
+	  absci1[2]=0.1742437375162050E+1;
+	  absci1[3]=0.2639813333635586E+1;
+	  wts1[0]=0.7649092266787873E-1;
+	  wts1[1]= 0.2435439494642453;
+	  wts1[2]= 0.1162953035510695;
+	  wts1[3]= 0.6783287043185401E-2;
+	  for (j1=0;j1<N1;j1++){
+	    wts1[j1]=wts1[j1]*exp(pow(absci1[j1],2.0));
+	  }
+	}break; 
+      case 8:   //8-gaussian for y^2*exp(-y^2)
+	{ absci1[0]=0.1990000637984294;
+	  absci1[1]=0.5059526450205794;
+	  absci1[2]=0.9041682182040568;
+	  absci1[3]=0.1372615723971598E+1;
+	  absci1[4]=0.1900969572329702E+1;
+	  absci1[5]=0.2490479841967435E+1;
+	  absci1[6]=0.3158780677105240E+1;
+	  absci1[7]=0.3966720403265353E+1;
+	  
+	  wts1[0]=0.9599144336400067E-2;
+	  wts1[1]=0.7072944976303661E-1;
+	  wts1[2]=0.157366887003943;
+	  wts1[3]=0.1429322724003870;
+	  wts1[4]=0.5431444004253597E-1;
+	  wts1[5]=0.7835224153141577E-2;
+	  wts1[6]=0.3338952597020048E-3;
+	  wts1[7]=0.2149767232664775E-5;
+	  for(j1=0;j1<N1;j1++){
+	    wts1[j1]=wts1[j1]*exp(pow(absci1[j1],2.0));
+	  }
+	}break;
+      case 16: //weights & absci for y^2 exp(-y^2)
+	{ absci1[0]= 0.8174913389984520E-1;
+	  absci1[1]=0.2154761962759740;
+	  absci1[2]=0.4003530517087630;
+	  absci1[3]=0.6298538771405607;
+	  absci1[4]=0.8976124329697087;
+	  absci1[5]=0.1198149260240153E+1;
+	  absci1[6]=0.1527188184719962E+1;
+	  absci1[7]=0.1881745606015598E+1;
+	  absci1[8]=0.2260132964654088E+1;
+	  absci1[9]=0.2661980315279350E+1;
+	  absci1[10]=0.3088376381635592E+1;
+	  absci1[11]=0.3542256017930265E+1;
+	  absci1[12]=0.4029312272760483E+1;
+	  absci1[13]=0.4560203031431090E+1;
+	  absci1[14]=0.5156826768007481E+1;
+	  absci1[15]=0.58781144889155572E+1;
+	  
+	  wts1[0]=0.7050727473210895E-3;
+	  wts1[1]=0.7107111654073120E-2;
+	  wts1[2]=0.2844188515941899E-1;
+	  wts1[3]=0.6660235171398239E-1;
+	  wts1[4]=0.1025785712747278;
+	  wts1[5]=0.1077502032531791;
+	  wts1[6]=0.7747156370638879E-1;
+	  wts1[7]=0.3763106373385135E-1;
+	  wts1[8]=0.1204873635560290E-1;
+	  wts1[9]=0.2453208613776865E-2;
+	  wts1[10]=0.3020309847850189E-3;
+	  wts1[11]=0.2092121075871870E-4;
+	  wts1[12]=0.7314637349679360E-6;
+	  wts1[13]=0.1080646863902574E-7;
+	  wts1[14]=0.4828081616137754E-10;
+	  wts1[15]=0.2840126937112534E-13;
+	  for (j1=0;j1<N1;j1++){
+	    wts1[j1]=wts1[j1]*exp(pow(absci1[j1],2.0));
+	  }
+	}break;
+      }
+      switch(option_theta){
+	
+      case 0:   //constant difference for Theta
+	{ double pi=3.14159;
+	  double dh2=2*pi/(N2); //number of intervals=no. of ordinates
+	  for (int j2=0;j2<N2;j2++){
+	    absci2[j2]= dh2*j2;
+	    wts2[j2]=dh2;
+	  }
+	}break;
+      case 1:  //three-eights for theta
+	{ double pi=3.14159;
+	  double h=pi;                     //the range of quadrature from [-h,h];
+	  double dh=2*h/n_int;             //anglular increment
+	  for (int i=0;i<n_int;i++){
+	    absci2[3*i]=-h+dh/3.0*(3.0*i);       
+	    absci2[3*i+1]=-h+dh/3.0*(3.0*i+1);   
+	    absci2[3*i+2]=-h+dh/3.0*(3.0*i+2);   
+	    if (i == 0){
+	      wts2[i]=dh/4.0;  //here 0 -pi and pi are the same number
+	    }
+	    else {
+	      wts2[3*i]=dh/4.0;
+	    }
+	    wts2[3*i+1]=dh/8.0*3.0;
+	    wts2[3*i+2]=dh/8.0*3.0;
+	  }
+	  //absci2(N2)=h;wts2(N2)=dh/8.0;
+	}break;
+      }
+      
+      switch(option_phi){
+	
+      case 0: //constant difference for Phi
+	{
+	  double pi=3.14159;
+	  double dh3=pi/(N3-1); //number of intervals < number of ordinates
+	  for (int j3=0;j3<N3;j3++){
+	    absci3[j3]= dh3*j3;
+	    wts3[j3]=dh3*sin(absci3[j3]);
+	  }
+	}break;
+      case 1: //three-eights for phi
+	{ double pi=3.14159;
+	  double dh_phi=pi/nphi_int;  //anglular increment
+	  for (int i=0;i<nphi_int;i++){
+	    absci3[3*i]=dh_phi/3.0*(3.0*i);      
+	    absci3[3*i+1]=dh_phi/3.0*(3.0*i+1);  
+	    absci3[3*i+2]=dh_phi/3.0*(3.0*i+2);  
+	    if (i == 0){  
+	      wts3[i]=dh_phi/8.0*sin(absci3[i]);
+	    }
+	    else{
+	      wts3[3*i]=dh_phi/4.0*sin(absci3[3*i]);
+	    }      
+	    wts3[3*i+1]=dh_phi/8.0*3.0*sin(absci3[3*i+1]);
+	    wts3[3*i+2]=dh_phi/8.0*3.0*sin(absci3[3*i+2]);
+	  }
+	  absci3[N3-1]=pi;
+	  wts3[N3-1]=dh_phi/8.0*sin(absci3[N3-1]);
+	}break;
+      }
+       int j;
+       j=0;
+       for (int j1=0;j1<N1;j1++){ //Ur
+	 for (int j2=0;j2<N2;j2++){ //theta
+	   for (int j3=0;j3<N3;j3++){ //phi
+	     cx[j]=absci1[j1]*cos(absci2[j2])*sin(absci3[j3]); //cx=Ur*cos(theta)*sin(phi)
+	     cy[j]=absci1[j1]*sin(absci2[j2])*sin(absci3[j3]); //cy=Ur*sin(theta)*sin(phi)
+	     cz[j]=absci1[j1]*cos(absci3[j3]);                 //Ur*cos(phi)
+	     dcxyz[j]=wts1[j1]*wts2[j2]*wts3[j3];
+	     j++;       
+	   }
+	 }
+       }
+       
     }
   /*quadrature(int& N1, int& N2, double& T2) :
     _var1(N1),
@@ -66,7 +289,8 @@ class quadrature
     logCtor();
     }*/
   
-  //virtual ~quadrature() {}
+  virtual ~quadrature() {}
 };
+
 
 #endif
--- src/fvm/src/modules/esbgkbase/testquadrature.cpp
+++ src/fvm/src/modules/esbgkbase/testquadrature.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "quadrature.h"
+#include <quadrature.h>
 using namespace std;
-
 
 int main(){
   int N1=8,N2=12,N3=10;
- double T2=1.0;
- quadrature myquad1 = quadrature(N1,N2,N3,T2); 
- 
+  double T2=1.0;
+  quadrature<double> myquad1 = quadrature<double>(N1,N2,N3,T2); 
+ int option_ur=8,Nr=8,option_theta=1,n_int =4, option_phi=1,nphi_int = 4;
+ quadrature<double> myquad2= quadrature<double>(option_ur,Nr,option_theta,n_int,option_phi,nphi_int);
 //double density=0.5;
  //double temperature=2.0;
  //double x_velocity=0.1;
