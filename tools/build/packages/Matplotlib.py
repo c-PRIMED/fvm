9190a3d02ebb8a4b3c25cc382db0288661f22168
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-02-01 23:56:09 +0000

    add scipy and matplotlib classes

---
+++ tools/build/packages/Matplotlib.py
@@ -0,0 +1,10 @@
+from build_packages import *
+
+class Matplotlib(BuildPkg):
+    requires = ['numpy']
+    def _install(self):
+        do_env("LDFLAGS=")
+        ret = self.sys_log("python setup.py install --prefix=%s" % self.blddir)
+        do_env("LDFLAGS=", unload=True)
+        return ret
+
