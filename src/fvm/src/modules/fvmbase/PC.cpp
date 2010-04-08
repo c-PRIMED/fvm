26cb4c1372771c2b1f870238270fc709ccbbf731
srmathur <srmathur@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-04-08 16:04:55 +0000

    initial version

---
+++ src/fvm/src/modules/fvmbase/PC.cpp
@@ -0,0 +1,15 @@
+#include "PC.h"
+
+
+PCSet*
+createPCSet(const int order, const int dim)
+{
+  PCSet *pc = new PCSet(order,dim,"HG");
+
+  cout << "The number of PC terms in an expansion is "
+       << pc->GetNumberPCTerms() << endl;
+
+
+  pc->SetTaylorTolerance(1.e-15);
+  return pc;
+}
