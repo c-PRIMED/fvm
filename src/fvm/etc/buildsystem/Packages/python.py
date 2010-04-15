69d0ec68118d88303995a641323e4e6a216afeb6
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-04-15 15:42:21 +0000

    better instructions for linking to python

--- src/fvm/etc/buildsystem/Packages/python.py
+++ src/fvm/etc/buildsystem/Packages/python.py
@@ -4,10 +4,11 @@ import Arch
 def generate(env):
     cv = get_config_vars()
     pylib = 'python'+get_python_version()
+    pylibpath = [cv[var] for var in ('LIBDIR', 'LIBPL')]
     env.Append(CPPPATH=[get_python_inc(),numpy.get_include()])
     if Arch.isWindows():
-        env.Append(LIBS=[pylib.replace(".","")])
-        env.Append(LIBPATH=os.path.join(cv['prefix'],"libs"))
+        env.Append(LIBS=pylib.replace(".",""))
+        env.Append(LIBPATH=pylibpath)
     else:
-        env.Append(LIBS=[pylib])
-        env.Append(LIBPATH=env['PACKAGESLIBDIR'])
+        env.Append(LIBS=pylib)
+        env.Append(LIBPATH=pylibpath)
