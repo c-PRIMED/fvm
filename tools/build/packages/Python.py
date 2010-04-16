2b98f6e38d064877934adeac054841a2fca110c8
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-04-16 17:32:53 +0000

    allow python builds in Build=True even if installed python is useable

--- tools/build/packages/Python.py
+++ tools/build/packages/Python.py
@@ -1,13 +1,15 @@
 from build_packages import *
+import config
 
 class Python(BuildPkg):    
     def _configure(self):
-        py = find_executable('python')
-        ver = subprocess.Popen('python -V', shell=True, stderr=subprocess.PIPE).stderr.read()        
-        a, b = re.compile(r'Python ([^.]*).([^.\n]*)').findall(ver)[0]
-        if int(a) == 2 and int(b) >= 5:
-            self.use_installed = True
-            return 'installed'
+        if config.config('python', 'Build') == '':
+            py = find_executable('python')
+            ver = subprocess.Popen('python -V', shell=True, stderr=subprocess.PIPE).stderr.read()        
+            a, b = re.compile(r'Python ([^.]*).([^.\n]*)').findall(ver)[0]
+            if int(a) == 2 and int(b) >= 5:
+                self.use_installed = True
+                return 'installed'
         self.use_installed = False
         return self.sys_log("%s/configure --prefix=%s --enable-shared" % (self.sdir, self.blddir))
     def _build(self):
