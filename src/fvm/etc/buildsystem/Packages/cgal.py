976076e1f60772962fda1d2f21402dd48c8eca06
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-06-22 16:28:41 +0000

    quick fix

--- src/fvm/etc/buildsystem/Packages/cgal.py
+++ src/fvm/etc/buildsystem/Packages/cgal.py
@@ -4,16 +4,18 @@ import os
 def generate(env):
 
     # coates prepends '-I' to CGI_INCLUDE
-    cgi = os.environ['CGAL_INCLUDE']
-    if cgi.startswith('-I'):
-        cgi = cgi.lstrip('-I')
-        
-    cgd = os.environ['CGAL_DIR']
-    if cgd.endswith('/CGAL'):
-        cgd = cgd.rstrip('/CGAL')
-        
+    try:
+        cgi = os.environ['CGAL_INCLUDE']
+        if cgi.startswith('-I'):
+            cgi = cgi.lstrip('-I')
+        cgd = os.environ['CGAL_DIR']
+        if cgd.endswith('/CGAL'):
+            cgd = cgd.rstrip('/CGAL')
+
+        if cgd:
+            env.Append(LIBPATH=cgd)
+        if cgi:
+            env.Append(CPPPATH=cgi)
+    except:
+        pass
     env.Append(LIBS=['CGAL'])
-    if cgd:
-        env.Append(LIBPATH=cgd)
-    if cgi:
-        env.Append(CPPPATH=cgi)
