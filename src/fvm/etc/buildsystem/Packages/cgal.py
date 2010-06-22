b3d15683b9dbcbba8a0df7728ab4424ed3c2f2f6
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-06-22 16:14:52 +0000

    Fixes to get builds working on coates

--- src/fvm/etc/buildsystem/Packages/cgal.py
+++ src/fvm/etc/buildsystem/Packages/cgal.py
@@ -1,6 +1,19 @@
 import distutils.sysconfig
-
+import os
 
 def generate(env):
-    env.Append(LIBS=['CGAL'])
 
+    # coates prepends '-I' to CGI_INCLUDE
+    cgi = os.environ['CGAL_INCLUDE']
+    if cgi.startswith('-I'):
+        cgi = cgi.lstrip('-I')
+        
+    cgd = os.environ['CGAL_DIR']
+    if cgd.endswith('/CGAL'):
+        cgd = cgd.rstrip('/CGAL')
+        
+    env.Append(LIBS=['CGAL'])
+    if cgd:
+        env.Append(LIBPATH=cgd)
+    if cgi:
+        env.Append(CPPPATH=cgi)
