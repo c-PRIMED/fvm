75ad566fcbf4866ef01b61643deea685dc3dd03b
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-07-08 17:06:31 +0000

    cleanup coupling script, creating surface mesh and send back stress from flowfields.force to MPM

--- src/fvm/scripts/FluidStructure.py
+++ src/fvm/scripts/FluidStructure.py
@@ -9,13 +9,12 @@ import time
 
 class MPMCoupling:
 
-     def __init__(self, MeshList, FlowModel, FlowField, GeomField, solid):
+     def __init__(self, FlowModel, flowFields, MeshList, GeomField):
          self.meshList      = MeshList
          self.mesh          = MeshList[0]  #for now!!!!!!!!!!!!!!!!!!!!!
-         self.flowModel = FlowModel
-         self.flowField = FlowField
          self.geomField = GeomField
-         self.solid     = solid
+	 self.flowModel = FlowModel
+	 self.flowFields = flowFields
          self.setup()
          
      def setup(self):
@@ -23,8 +22,6 @@ class MPMCoupling:
          self.dtMPM = zeros(1,float);
 	 self.dt      = zeros(1,float)
          self.ndim = int(3)
-         self.xc     = self.geomField.coordinate[self.mesh.getCells()].asNumPyArray()
-         self.volume = self.geomField.volume[self.mesh.getCells()].asNumPyArray()
 
          self.procID = MPI.COMM_WORLD.Get_rank();
          #connect to server first get parent and port name then connect to port name
@@ -41,9 +38,6 @@ class MPMCoupling:
          print " self.portName = ", self.portName.tostring();
          #Connect this port
          self.FVM_COMM_MPM = MPI.COMM_WORLD.Connect(self.portName.tostring().rstrip('\0'), MPI.INFO_NULL, root=0) 
-         #construct FVMParticles class
-         self.fvmParticles = fvmbaseExt.FVMParticles( self.meshList );
-         self.totParticlesFVM = zeros(1,int32);
          
          self.remoteSize   = self.FVM_COMM_MPM.Get_remote_size()
          self.nfaces       = zeros( self.remoteSize, dtype='i' )
@@ -62,8 +56,8 @@ class MPMCoupling:
 	      mpi_proc = MPI.PROC_NULL
 	      if ( self.procID == 0 ):
 	          mpi_proc = MPI.ROOT
-              self.FVM_COMM_MPM.Bcast([self.dt,MPI.DOUBLE], root=MPI.ROOT)	
-              #self.dtMPM = self.dtMPM / 1000.0 #from "msec" to "second" conversion
+              self.FVM_COMM_MPM.Bcast([self.dt,MPI.DOUBLE], mpi_proc)	
+              self.dtMPM = self.dtMPM / 1000.0 #from "msec" to "second" conversion
 	      a = int(round(self.dt/self.dtMPM))
 	      b = int(round(self.dtMPM/self.dt))
 	      #coupling if mod(step, couplingStep)=0
@@ -82,39 +76,13 @@ class MPMCoupling:
      def updateMPM(self, istep):
         
          if (  istep%self.couplingStep == 0  ):
-             #update fluid particles particles 
-             mesh0 = int(0)   
 	     totCentroids = self.nfaces.sum()
-             self.stress  = zeros((totCentroids,3),float) #self.geomField.coordinate[self.mesh.getCells()].asNumPyArray().copy()
-             recvbuf      = zeros((totCentroids,3),float) #self.geomField.coordinate[self.mesh.getCells()].asNumPyArray().copy()
-
-	     #compute cell centroids to store fake 
-	     indx  = int(0)
-	     for i in range(0,totCentroids):
-		 sumCoordX = 0.0
-		 sumCoordY = 0.0
-		 sumCoordZ = 0.0
-	         for j in range(0,4):
-		     sumCoordX = sumCoordX + self.faceNodesCoord.asNumPyArray()[indx,0]
-		     sumCoordY = sumCoordY + self.faceNodesCoord.asNumPyArray()[indx,1]
-		     sumCoordZ = sumCoordZ + self.faceNodesCoord.asNumPyArray()[indx,2]
-		     indx = indx + 1
-		 self.stress[i,0] = 0.25 * sumCoordX
-		 self.stress[i,1] = 0.25 * sumCoordY
-		 self.stress[i,2] = 0.25 * sumCoordZ
-		 recvbuf[i,0] = 0.25 * sumCoordX
-		 recvbuf[i,1] = 0.25 * sumCoordY
-		 recvbuf[i,2] = 0.25 * sumCoordZ
+             self.stress  = self.flowFields.force[self.surfaceMeshes[0].getFaces()].asNumPyArray().copy()
+             recvbuf      = self.flowFields.force[self.surfaceMeshes[0].getFaces()].asNumPyArray().copy()
 		     
              self.FVM_COMM_MPM.Allreduce( [self.stress, MPI.DOUBLE], [recvbuf,MPI.DOUBLE], op=MPI.SUM)
-             #stress at FVM particles
-             #self.cellIDs  = self.fvmParticles.getCellIDs( mesh0 )
-             #self.stress = self.flowModel.getStressTensor( self.meshList[ mesh0], self.cellIDs ).asNumPyArray().copy()
-             #fileMaxPressure = open("max_pressure.dat",'a')
-             #fileMaxPressure.write( str( abs(self.stress).max() ) + "\n" )
-             #fileMaxPressure.close()
 
-             print "fluid particlesStress(11) = ", self.stress[11][0], "  ",   self.stress[11][1], "  ", \
+             print "face Stress(11) = ", self.stress[11][0], "  ",   self.stress[11][1], "  ", \
                    self.stress[11][2], " \n "
 
 
@@ -133,34 +101,35 @@ class MPMCoupling:
                 #creating fvm array 
                 self.faceNodesCoord =  self.geomField.coordinate[self.mesh.getCells()].newSizedClone( self.nfaces.sum()*4 )
                 self.FVM_COMM_MPM.Allgatherv([None,0,0,MPI.DOUBLE],[self.faceNodesCoord.asNumPyArray(),count, displ,MPI.DOUBLE]) 
-                #print "MPI_RANK = ", MPI.COMM_WORLD.Get_rank(), "  faceNodes coord = ", self.faceNodesCoord.asNumPyArray()                 
+		print "self.nfaces = ", self.nfaces
+                print "MPI_RANK = ", MPI.COMM_WORLD.Get_rank(), "  faceNodes coord = ", self.faceNodesCoord.asNumPyArray()                 
+		self.faceNodesCoord.asNumPyArray()[:,:] = self.faceNodesCoord.asNumPyArray()[:,:] / 1000.0
+                #creating meshList 
+                #self.surfaceMeshes= fvmbaseExt.MeshList( fvmbaseExt.Mesh(3,99,self.faceNodesCoord) );
+		self.surfaceMeshes = [ fvmbaseExt.Mesh(3,99,self.faceNodesCoord) ]
                 
+		#self.surfaceMesh = fvmbaseExt.Mesh(3,99,self.faceNodesCoord)
                 #count
                 count = self.nfaces * 3 
                 #displ
-                displ = zeros( len(count), dtype='i')
+                displ = zeros(len(count), dtype='i')
                 #filling displ
                 displ[0] = 0
                 for i in range(1,len(count)):
                    displ[i] = displ[i-1] + count[i-1]
                 #creating fvm array 
                 self.FaceCentroidVels =  self.geomField.coordinate[self.mesh.getCells()].newSizedClone( self.nfaces.sum() )
-		#print "facecentroidvels = ", self.FaceCentroidVels.asNumPyArray(), "   rank = ", MPI.COMM_WORLD.Get_rank()
+		print "facecentroidvels = ", self.FaceCentroidVels.asNumPyArray(), "   rank = ", MPI.COMM_WORLD.Get_rank()
                 self.FVM_COMM_MPM.Allgatherv([None,0,0,MPI.DOUBLE],[self.FaceCentroidVels.asNumPyArray(),count, displ,MPI.DOUBLE]) 
 		if  MPI.COMM_WORLD.Get_rank() == 0:
 		    self.dump_faces(self.faceNodesCoord.asNumPyArray(), self.FaceCentroidVels.asNumPyArray())
 
-     def  particleSite(self):
-         return self.particles 
+     def  getSurfaceMeshes(self):
+         return self.surfaceMeshes
+  
+     def  getSurfaceVel(self):
+         return self.FaceCentroidVels
 
-     def getCoord(self):
-         return self.coordP.copy('C')
-
-     def getVel(self):
-         return self.velP.copy('C')
-     
-     def getNparticles( self):
-         return int(self.nparticles[0])
 
      def dump_faces( self, faceNodesCoord, faceVel ):
         f = open('faces.dat','w')
@@ -207,45 +176,6 @@ class MPMCoupling:
 
         f.close()   
 	 
-     def   dump_coord_fluid(self, nparticles, px ):
-        fx = open('px_fluid.dat','w')
-        fx.write( str(nparticles))
-        fx.write("\n")
-        for n in range(0,nparticles):
-            fx.write(str(px[n,0]) )
-            fx.write("     ")
-            fx.write(str(px[n,1]) )
-            fx.write("     ")
-            fx.write(str(px[n,2]) )
-            fx.write("\n")
-        fx.close()
-     
-     def   dump_coord_vel(self, nparticles, px, pv):
-        fx = open('px_solid.dat','w')
-        fx.write(str(nparticles))
-        fx.write("\n")
-        for n in range(0,nparticles):
-            fx.write(str(px[n,0]) )
-            fx.write("     ")
-            fx.write(str(px[n,1]) )
-            fx.write("     ")
-            fx.write(str(px[n,2]) )
-            fx.write("\n")
-        fx.close()
-
-        fv = open('pv_solid.dat','w')
-        fv.write(str(nparticles))
-        fv.write("\n")
-        for n in range(0,nparticles):
-            fv.write(str(pv[n,0]) )
-            fv.write("     ")
-            fv.write(str(pv[n,1]) )
-            fv.write("     ")
-            fv.write(str(pv[n,2]) )
-            fv.write("\n")
-        fv.close()
-
-
      def __del__(self):
        self.FVM_COMM_MPM.Barrier()
        self.FVM_COMM_MPM.Disconnect()
