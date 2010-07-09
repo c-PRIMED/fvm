9452d123180282a3ce95f9a3fce24857e405a2ae
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-07-09 23:06:40 +0000

    more uq restructuring

--- config/packages/MEMOSA.py
+++ config/packages/MEMOSA.py
@@ -1,14 +1,13 @@
 from build_packages import *
 
 class MEMOSA(BuildPkg):
-    requires = ['mpi4py', 'h5py', 'scipy', 'matplotlib', 'nose', 'pystatistics']
+    requires = ['mpi4py', 'h5py', 'nose', 'puq']
     copy_sources = 1
     name = "MEMOSA"
     def _build(self):
         return self.sys_log("make -j%s" % (jobs(self.name)))
     def _install(self):
         os.chdir(self.bdir)
-        self.sys_log("install src/sparse_grid_cc %s" % self.bindir)
         self.sys_log("install src/xyz2xdmf %s" % self.bindir)
         os.chdir(self.sdir)
         self.sys_log("install bin/* %s" % self.bindir)
