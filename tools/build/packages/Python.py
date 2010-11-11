86f9a2c97f906c37298309adbd929a8794c3d287
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-11-11 18:14:28 +0000

    cannot use python2.7 yet

--- tools/build/packages/Python.py
+++ tools/build/packages/Python.py
@@ -1,13 +1,13 @@
 from build_packages import *
 import config
 
-class Python(BuildPkg):    
+class Python(BuildPkg):
     def _configure(self):
         if config.config('python', 'Build') == '':
             py = find_executable('python')
-            ver = subprocess.Popen('python -V', shell=True, stderr=subprocess.PIPE).stderr.read()        
+            ver = subprocess.Popen('python -V', shell=True, stderr=subprocess.PIPE).stderr.read()
             a, b = re.compile(r'Python ([^.]*).([^.\n]*)').findall(ver)[0]
-            if int(a) == 2 and int(b) >= 5:
+            if int(a) == 2 and int(b) >= 5 and int(b) < 7:
                 self.use_installed = True
                 return 'installed'
         self.use_installed = False
@@ -17,7 +17,7 @@ class Python(BuildPkg):
             return "skip"
         return self.sys_log("make")
     def _install(self):
-        BuildPkg.pypath = set_python_path(self.blddir)        
-        if self.use_installed:            
+        BuildPkg.pypath = set_python_path(self.blddir)
+        if self.use_installed:
             return "Skip"
         self.sys_log("make install")
