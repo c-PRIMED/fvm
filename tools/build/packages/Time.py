4f06c13e2f69359e122136880f766dde1b4c4b07
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2010-12-01 16:17:43 +0000

    add time

---
+++ tools/build/packages/Time.py
@@ -0,0 +1,11 @@
+from build_packages import *
+
+# Build GNU time command, because some systems only have BSD time.
+
+class Time(BuildPkg):
+    def _configure(self):
+        return self.sys_log("%s/configure -prefix=%s" % (self.sdir, self.blddir))
+    def _build(self):
+        return self.sys_log("make -j%s" % jobs(self.name))
+    def _install(self):
+        return self.sys_log("make install")
