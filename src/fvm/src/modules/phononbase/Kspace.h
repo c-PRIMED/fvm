476a9e9dfce33c208b69f5efeccac86f7ec05761
jloy <jloy@8639bca7-a847-0410-9c84-802de9f82a5b>
2011-05-11 14:36:46 +0000

    Changed the interface file from having %include "Kspace.h" to a stripped down version of Kspace.h added NewtonSolve, gete0_tau, calcSpecificHeat, and findSpecs

--- src/fvm/src/modules/phononbase/Kspace.h
+++ src/fvm/src/modules/phononbase/Kspace.h
@@ -19,12 +19,15 @@ class Kspace
   typedef kvol<T> Tkvol;
   typedef shared_ptr<Tkvol> Kvolptr;
   typedef vector<Kvolptr> Volvec;
+  typedef typename Tmode::Reflection Reflection;
+  typedef typename Tmode::Reflptr Reflptr;
+  typedef typename Tmode::Refl_pair Refl_pair;
+  typedef typename Tmode::Refl_Map Refl_Map;
 
- Kspace(T tau,T vgmag,T cp,int ntheta,int nphi) :
+ Kspace(T a, T tau, T vgmag, T omega, int ntheta, int nphi) :
   _length(ntheta*nphi),
     _Kmesh(),
-    _totvol(0.),
-    _Covertau(0.)
+    _totvol(0.)
       { //makes gray, isotropic kspace  
 	
 	const double pi=3.141592653;
@@ -44,14 +47,12 @@ class Kspace
 		vg[1]=vgmag*sin(theta)*cos(phi);
 		vg[2]=vgmag*cos(theta);
 		dk3=2.*sin(theta)*sin(dtheta/2.)*dphi;
-		Tmodeptr modeptr=shared_ptr<Tmode>(new Tmode(vg,cp,tau));
+		Tmodeptr modeptr=shared_ptr<Tmode>(new Tmode(vg,omega,tau));
 		Kvolptr volptr=shared_ptr<Tkvol>(new Tkvol(modeptr,dk3));
 		_Kmesh.push_back(volptr);
-		_Covertau+=cp/tau*dk3;
 		_totvol+=dk3;
 	      }
 	  }
-	_Covertau=_Covertau/_totvol;
       }
   
   //void setvol(int n,Tkvol k) {*_Kmesh[n]=k;}
@@ -62,8 +63,126 @@ class Kspace
     return (_Kmesh[0]->getmodenum())*_length;
   }
   T getDK3() const {return _totvol;}
-  T getCovertau() const {return _Covertau;}
+  void NewtonSolve(T& guess, const T e_sum)
+  {
+    T e0_tau;
+    T de0_taudT;
+    T deltaT=1.;
+    T newguess;
+    int iters=0;
+    while(deltaT>1e-6)
+      {
+	gete0_tau(guess,e0_tau,de0_taudT);
+	deltaT=(e0_tau-e_sum)/de0_taudT;
+	newguess=guess-deltaT;
+	deltaT=deltaT/guess;
+	guess=newguess;
+	iters++;
+      }	
+  }
 
+  void gete0_tau(T& Tguess, T& e0tau, T& de0taudT)
+  {
+    e0tau=0.;
+    de0taudT=0.;
+    for(int k=0;k<_length;k++)
+      {
+	Tkvol& kv=getkvol(k);
+	const int modenum=kv.getmodenum();
+	T dk3=kv.getdk3();
+	for(int m=0;m<modenum;m++)
+	  {
+	    Tmode& mode=kv.getmode(m);
+	    e0tau+=mode.calce0tau(Tguess)*dk3;
+	    de0taudT+=mode.calcde0taudT(Tguess)*dk3;
+	  }
+      }
+  }
+
+  T calcSpecificHeat(T Tl)
+  {
+    T r(0.0);
+    for(int k=0;k<_length;k++)
+      {
+	Tkvol& kv=getkvol(k);
+	const int modenum=kv.getmodenum();
+	T dk3=kv.getdk3();
+	for(int m=0;m<modenum;m++)
+	  {
+	    Tmode& mode=kv.getmode(m);
+	    r+=mode.calcde0dT(Tl)*dk3;
+	  }
+      }
+    return r;
+  }
+
+  void findSpecs(T dk3, T vo, int m, Tvec so, Refl_pair& refls)
+  {
+    Tmode& mode1=getkvol(0).getmode(m);
+    Tmode& mode2=getkvol(1).getmode(m);
+    int m1=0;
+    int m2=1;
+    Tvec vg1=mode1.getv();
+    Tvec vg2=mode2.getv();
+    Tvec sn1=vg1/sqrt(pow(vg1[0],2)+pow(vg1[1],2)+pow(vg1[2],2));
+    Tvec sn2=vg2/sqrt(pow(vg2[0],2)+pow(vg2[1],2)+pow(vg2[2],2));
+    T sn1dotso=sn1[0]*so[0]+sn1[1]*so[1]+sn1[2]*so[2];
+    T sn2dotso=sn2[0]*so[0]+sn2[1]*so[1]+sn2[2]*so[2];
+    
+    if (sn2dotso>sn1dotso)
+      {
+	T temp=sn1dotso;
+	sn1dotso=sn2dotso;
+	sn2dotso=temp;
+	m1=1;
+	m2=0;
+      }
+
+    for(int k=2;k<_length;k++)
+      {
+	Tmode& temp_mode=getkvol(k).getmode(m);
+	Tvec vgt=temp_mode.getv();
+	const Tvec sn=vgt/sqrt(pow(vgt[0],2)+pow(vgt[1],2)+pow(vgt[2],2));
+	const T sndotso=sn[0]*so[0]+sn[1]*so[1]+sn[2]*so[2];
+	
+	if(sndotso>sn1dotso)
+	  {
+	    sn2dotso=sn1dotso;
+	    sn1dotso=sndotso;
+	    m2=m1;
+	    m1=k;
+	  }
+	else if (sndotso>sn2dotso)
+	  {
+	    sn2dotso=sndotso;
+	    m2=k;
+	  }
+      }
+    
+    T w1=sn1dotso/(sn1dotso+sn2dotso);
+    T w2=sn2dotso/(sn1dotso+sn2dotso);
+
+    if(sn1dotso>.99)
+      {
+	w1=1;
+	w2=0;
+      }
+
+    T dk31=getkvol(m1).getdk3();
+    T dk32=getkvol(m2).getdk3();
+
+    vg1=getkvol(m1).getmode(m).getv();
+    vg2=getkvol(m2).getmode(m).getv();
+
+    T v1mag=sqrt(pow(vg1[0],2)+pow(vg1[1],2)+pow(vg1[2],2));
+    T v2mag=sqrt(pow(vg2[0],2)+pow(vg2[1],2)+pow(vg2[2],2));
+
+    refls.first.first=w1*vo*dk3/v1mag/dk31;
+    refls.second.first=w2*vo*dk3/v2mag/dk32;
+    refls.first.second=m1;
+    refls.second.second=m2;  
+  }
+  
  private:
 
   Kspace(const Kspace&);
@@ -71,9 +190,7 @@ class Kspace
   int _length;
   Volvec _Kmesh;
   T _totvol;    //total Kspace volume
-  T _Covertau;
   
-
 };
 
 
--- src/fvm/src/modules/phononbase/Kspace.i
+++ src/fvm/src/modules/phononbase/Kspace.i
@@ -2,6 +2,39 @@
 #include "Kspace.h"
   %}
 
-%include "Kspace.h"
+
+template<class T>
+class Kspace
+{
+
+ public:
+
+  typedef Vector<T,3> Tvec;
+  typedef pmode<T> Tmode;
+  typedef shared_ptr<Tmode> Tmodeptr;
+  typedef kvol<T> Tkvol;
+  typedef shared_ptr<Tkvol> Kvolptr;
+  typedef vector<Kvolptr> Volvec;
+  typedef typename Tmode::Reflection Reflection;
+  typedef typename Tmode::Reflptr Reflptr;
+  typedef typename Tmode::Refl_pair Refl_pair;
+  typedef typename Tmode::Refl_Map Refl_Map;
+
+  Kspace(T a, T tau, T vgmag, T omega, int ntheta, int nphi) ;
+  int getlength();
+  int gettotmodes();
+  T getDK3() ;
+  T calcSpecificHeat(T Tl);
+  
+ private:
+  
+  Kspace(const Kspace&);
+  //num volumes
+  int _length;
+  Volvec _Kmesh;
+  T _totvol;    //total Kspace volume
+  
+};
+
 
 %template(KspaceA) Kspace< ATYPE_STR >;
