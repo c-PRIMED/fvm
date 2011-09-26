1d6911331dc451ee4b062742629f01dee77e2b29
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2011-09-26 04:40:36 +0000

    update sympy and ipython packages

--- tools/build/packages/Ipython.py
+++ tools/build/packages/Ipython.py
@@ -1,13 +1,10 @@
 from build_packages import *
 
-# actually a collection of required packages for ipython
 class Ipython(BuildPkg):
     requires = ['numpy']
-    def _configure(self):
-        cfgname = os.path.join(self.bdir,'pkginstall.cfg')
-        cf = open(cfgname, 'w')
-        cf.write('PREFIX=%s\n' % self.blddir)
-        cf.close()
-        return 0
-    def _build(self):
-        return self.sys_log("make")
+    def _install(self):
+        do_env("LDFLAGS=")
+        ret = self.sys_log("python setup.py install --prefix=%s" % self.blddir)
+        do_env("LDFLAGS=", unload=True)
+        return ret
+
