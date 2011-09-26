147209d805147613f3b7cb37a99c22f4bbe7f77d
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2011-09-26 04:48:22 +0000

    update sympy and ipython packages

--- config/packages/Puq.py
+++ config/packages/Puq.py
@@ -6,16 +6,16 @@ class Puq(BuildPkg):
     def _build(self):
         return self.sys_log("make -j%s" % (jobs(self.name)))
     def _install(self):
-        os.chdir(os.path.join(self.bdir, 'adap', 'src'))        
+        os.chdir(os.path.join(self.bdir, 'adap', 'src'))
         self.sys_log("python setup.py install --prefix=%s" % self.blddir)
         os.chdir(self.bdir)
         self.sys_log("install src/sparse_grid_cc %s" % self.bindir)
         os.chdir(self.sdir)
         self.sys_log("install bin/* %s" % self.bindir)
         self.sys_log("install -d %s" % os.path.join(self.libdir, 'puq'))
-        self.sys_log("install -d %s" % os.path.join(self.libdir, 'puqutil'))        
+        self.sys_log("install -d %s" % os.path.join(self.libdir, 'puqutil'))
         self.sys_log("install -m 644 lib/*.py %s" % os.path.join(self.libdir, 'puq'))
-        self.sys_log("install -m 644 puqutil/*.py %s" % os.path.join(self.libdir, 'puqutil'))        
+        self.sys_log("install -m 644 puqutil/*.py %s" % os.path.join(self.libdir, 'puqutil'))
 
         idir = os.path.join(self.blddir, "include")
         if not os.path.isdir(idir):
