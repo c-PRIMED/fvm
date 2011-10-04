285986ff38d04d97bc78d623f9b944a606fe3986
sunlin <sunlin@8639bca7-a847-0410-9c84-802de9f82a5b>
2011-10-04 15:22:01 +0000

    correct damping force computation

--- src/fvm/scripts/ComputeForce.py
+++ src/fvm/scripts/ComputeForce.py
@@ -65,14 +65,18 @@ def computeDampCoeff(gap, width, thickness, pressure=1.01325e5, stop=100e-9):
     if gap < stop:
     	gap = stop
     temperature = 300
-    k = 1.38e-23 
-    diameter = 4e-10
+    k = 1.3806503e-23 
+    diameter = 4.17e-10
     area = pi * diameter * diameter
     gama = k*temperature/area
     mfp = gama / pressure
+    Tref = 273.15
+    omega = 0.81
 
-    x1 = width / gap
-    x2 = mfp / width
+    Kn = k * temperature / (gap*sqrt(2)*area*pressure*pow(Tref/temperature, omega-0.5))
+
+    x1 = width/gap
+    x2 = Kn/x1
     Cf = A*pow(x1,c) * thickness / (1+B*pow(x1,d)*pow(x2,e))
 
     return Cf
