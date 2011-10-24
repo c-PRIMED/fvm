3e7ddc735312ab3b00f9a69fa0285e65ffeebd2a
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2011-10-24 18:54:42 +0000

    add numexpr for pytables

--- tools/build/packages/Pytables.py
+++ tools/build/packages/Pytables.py
@@ -1,7 +1,7 @@
 from build_packages import *
 
 class Pytables(BuildPkg):
-    requires = ['h5py', 'numpy']
+    requires = ['h5py', 'numexpr']
     def _install(self):
         hdf5 = self.bld.pkglist['hdf5']
         assert(hdf5)
