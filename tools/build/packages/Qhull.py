f02c43c0b8e2903af4cdbd49c6bba05f41d5a79a
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-01-06 21:20:15 +0000

    add qhull

---
+++ tools/build/packages/Qhull.py
@@ -0,0 +1,12 @@
+from build_packages import *
+
+class Qhull(BuildPkg):
+    requires = ['hdf5']    
+    def _configure(self):
+        return self.sys_log("cmake %s -DCMAKE_INSTALL_PREFIX=%s -DCMAKE_BUILD_TYPE=Release -DLATEX_OUTPUT_PATH=/tmp" % (self.sdir, self.blddir))
+    def _build(self):
+        return self.sys_log("make -j%s" % jobs(self.name))
+    def _install(self):
+        return self.sys_log("make install")
+
+
