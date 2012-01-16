4276461d2cfb880980b9e4f7d403fc464cad1603
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-01-16 20:49:34 +0000

    Eigen build fix

--- tools/build/packages/Eigen.py
+++ tools/build/packages/Eigen.py
@@ -4,5 +4,7 @@ import shutil
 class Eigen(BuildPkg):
     def _install(self):
         for idir in ['Eigen', 'unsupported']:
-            shutil.copytree(idir, os.path.join(self.blddir, "include", idir))
+            dname = os.path.join(self.blddir, "include", idir)
+            shutil.rmtree(dname, ignore_errors=1)
+            shutil.copytree(idir, dname)
         return 0
