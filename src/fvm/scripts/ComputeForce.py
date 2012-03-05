68d4ad62411b33e2137f34246be9007f3075351a
sunlin <sunlin@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-03-05 15:07:07 +0000

    contact force update

--- src/fvm/scripts/ComputeForce.py
+++ src/fvm/scripts/ComputeForce.py
@@ -12,6 +12,7 @@ dc = 110e-9
 ds = 30e-9
 d_bar = 1e-9
 tR = 8.5e-9
+ta = 2e-9
 
 def func(x):
     dx = 0
@@ -21,7 +22,7 @@ def func(x):
         dx = -x
     return dx
 ########################################################################
-def computeElectrostaticForce(voltage, distance, stop=100e-9):
+def computeElectrostaticForce(voltage, distance, stop=200e-9):
     # assume the distance is not smaller than 1nm
     if distance < stop:
         distance = stop
@@ -36,19 +37,22 @@ def computeElectrostaticForce(voltage, distance, stop=100e-9):
 # thickness1: dielectric thickness, i.e. thickness1 = 200e-9
 # src: charge density * QE 
 def computeElectrostaticForceWithCharge(voltage, dc0=1.0, thickness0=0., 
-                                        dc1=7.9, thickness1=200e-9, src=0.):
+                                        dc1=7.9, thickness1=200e-9, src=0., stop=100e-9):
+    if thickness0 < stop:
+        thickness0 = stop
     alpha0 = thickness0/dc0
     alpha1 = thickness1/dc1
     source = 0.5 * src * thickness1 * thickness1 / (dc1*epsilon0)
     ef = (voltage+source)/(alpha0+alpha1)
     elecForce = 0.5 * ef * ef * epsilon0
+    
     return elecForce
 
 ########################################################################
 def computeContactForce(x):
     #if distance < 1e-10:
     #    distance = 1e-10
-    attractive_force = -H/(6*pi*(pow(x,3)+pow(ds,3))) -c3*func(x-(dc+tR))/d_bar
+    attractive_force = -H/(6*pi*(pow(x,3)+pow(ds,3))) -c3*func(x-(dc+ta))/d_bar
     repulsive_force = c1*pow((func(x-dc)/d_bar),1.5) + c2*exp(-x/tR)
     contact_force = attractive_force + repulsive_force
     return contact_force
@@ -59,7 +63,7 @@ def computeDampForce(velocity, coeff):
     return dampForce
 
 ########################################################################
-def computeDampCoeff(gap, width, thickness, pressure=1.01325e5, stop=100e-9):
+def computeDampCoeff_Old(gap, width, thickness, pressure=1.01325e5, stop=200e-9):
     A = 10.39
     B = 1.374
     c = 3.100
@@ -82,4 +86,31 @@ def computeDampCoeff(gap, width, thickness, pressure=1.01325e5, stop=100e-9):
     x2 = Kn/x1
     Cf = A*pow(x1,c) * thickness / (1+B*pow(x1,d)*pow(x2,e))
 
+    return Cf
+#############################################################################
+def computeDampCoeff(gap, width, thickness, pressure=1.01325e5):
+    temperature = 300
+    k = 1.3806503e-23 
+    dref = 4.17e-10
+    Tref = 273.15
+    omega = 0.74
+    mfp = k*temperature /(sqrt(2.0)*pi*dref*dref*pressure*pow(Tref/temperature, omega-0.5))
+    Kn = mfp/gap
+
+    x_cut =  0.015
+    mu    =  1.98e-5
+    c1    =  5.5964
+    c2    =  (48.62*Kn)/(1+0.3405*Kn)
+    c3    =  (1+210.79*Kn)/(67.59*Kn)
+
+    x1    =  width/gap
+    x1_sqr = x1*x1
+    x1_cube = x1*x1*x1
+
+    if (1.0/x1 >= x_cut):
+        Cf = mu * x1_cube / (1 + c1*6.0*Kn) * (1+6.0*c2/x1+12.0*c3/x1_sqr)
+    else:
+        A1 = mu / (1+c1*6.0*Kn) * (-3.0/pow(x_cut, 4)-12.0*c2/pow(x_cut, 3)-12.0*c3/pow(x_cut, 2))
+        A2 = mu / (1+c1*6.0*Kn) * (1/pow(x_cut,3) + 6.0*c2/pow(x_cut,2) + 12.0*c3/x_cut )-x_cut*A1
+        Cf = A1/x1 + A2
     return Cf
