3e907def8397eaaeffd594bb60e7206a21c61b0b
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-05-21 20:09:17 +0000

    This is long time bug for very high number of processors's run. One or more processors somehow failed to open file in the very high number of processor run. This fix retry to open file if it is not succesful.

--- src/fvm/src/modules/importers/Reader.cpp
+++ src/fvm/src/modules/importers/Reader.cpp
@@ -1,4 +1,9 @@
 #include "Reader.h"
+#include <errno.h>
+
+#ifdef FVM_PARALLEL
+#include <mpi.h>
+#endif
 
 
 Reader::Reader(const string& fileName) :
@@ -16,9 +21,29 @@ Reader::~Reader()
 void
 Reader::resetFilePtr()
 {
-  if (_fp)
-    fclose(_fp);
-  _fp = fopen(_fileName.c_str(),"rb");
+
+  _fp = NULL;
+   int numOfAttempts = 10;
+  while( _fp == NULL && numOfAttempts > 0){
+//     if (_fp==NULL)
+//       fclose(_fp);
+     _fp = fopen(_fileName.c_str(),"rb");
+      if (_fp == NULL) 
+         printf("Error opening file %s: errno=%d %m", _fileName.c_str(), errno);
+      numOfAttempts--;
+  }
+ 
+  if ( _fp == NULL){
+     //parallel
+     #ifdef FVM_PARALLEL
+     const int mpi_err=99;
+     MPI::COMM_WORLD.Abort(mpi_err);
+     #endif
+     //serial
+     #ifndef FVM_PARALLEL
+     abort();
+     #endif
+  }
 }
 
 string
