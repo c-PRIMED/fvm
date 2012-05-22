12f96c4ded9469514e8eb66e6733e701b6d16e7d
yildirim <yildirim@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-05-22 19:18:37 +0000

    Adding to check if the file is open,

--- src/fvm/src/modules/importers/Reader.cpp
+++ src/fvm/src/modules/importers/Reader.cpp
@@ -21,12 +21,12 @@ Reader::~Reader()
 void
 Reader::resetFilePtr()
 {
+   if (_fp)
+      fclose(_fp);
 
   _fp = NULL;
    int numOfAttempts = 10;
   while( _fp == NULL && numOfAttempts > 0){
-//     if (_fp==NULL)
-//       fclose(_fp);
      _fp = fopen(_fileName.c_str(),"rb");
       if (_fp == NULL) 
          printf("Error opening file %s: errno=%d %m", _fileName.c_str(), errno);
