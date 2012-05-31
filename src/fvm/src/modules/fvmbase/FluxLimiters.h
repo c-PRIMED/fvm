c61694b34801a33fdbe23626cb5b80ce3e9ab375
jloy <jloy@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-05-31 20:33:47 +0000

    various functions for flux limiters

---
+++ src/fvm/src/modules/fvmbase/FluxLimiters.h
@@ -0,0 +1,27 @@
+#ifndef _FLUXLIMITERS_H_
+#define _FLUXLIMITERS_H_
+
+#include <math.h>
+
+template<class T>
+T minMod(const T r)
+{return max(0., min(1., r));}
+
+template<class T>
+T vanLeer(const T r)
+{return (r+fabs(r))/(1+fabs(r));}
+
+template<class T>
+T superbee(const T r)
+{return max(0., max(min(1., 2.*r), min(2., r)));}
+
+template<class T>
+T vanAlbada1(const T r)
+{return (pow(r,2)+r)/(pow(r,2)+1.);}
+
+template<class T>
+T ospre(const T r)
+{return 1.5*(pow(r,2)+r)/(pow(r,2)+r+1.);}
+
+
+#endif
