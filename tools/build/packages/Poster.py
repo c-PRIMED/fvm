cf08aabb81fac210b38e48eec377e288d39b643c
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2012-07-19 17:11:29 +0000

    fix Poster install for systems without setuptools

--- tools/build/packages/Poster.py
+++ tools/build/packages/Poster.py
@@ -3,6 +3,9 @@ from build_packages import *
 class Poster(BuildPkg):
     requires = ['python']
     def _install(self):
-        ret = self.sys_log("python setup.py install --prefix=%s" % self.blddir)
-        return ret
+        #ret = self.sys_log("python setup.py install --prefix=%s" % self.blddir)
+        os.chdir(self.sdir)
+        self.sys_log("install -d %s" % os.path.join(self.libdir, 'poster'))
+        self.sys_log("install -m 644 poster/*.py %s" % os.path.join(self.libdir, 'poster'))
+        return 0
 
