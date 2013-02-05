b2554b99d3eeeae8f8bbc2c2060c72f67b5d4d59
mmh <mmh@8639bca7-a847-0410-9c84-802de9f82a5b>
2013-02-05 18:12:18 +0000

    revert previous

--- tools/build/packages/Ufconfig.py
+++ tools/build/packages/Ufconfig.py
@@ -5,11 +5,10 @@ class Ufconfig(BuildPkg):
     def _build(self):
         return self.sys_log("INSTALL_LIB=%s INSTALL_INCLUDE=%s make" % (self.libdir, self.incdir))
     def _install(self):
+        # Need to rename the unpack directory because UMFPACK
+        # is hardcoded to look for it with a certain name
         newdir = os.path.join(os.path.split(self.sdir)[0], 'UFconfig')
-        if not os.path.isdir(newdir):
-            # Need to rename the unpack directory because UMFPACK
-            # is hardcoded to look for it with a certain name
-            shutil.move(self.sdir, newdir)
-            self.sdir = newdir
-            self.bdir = newdir
+        shutil.move(self.sdir, newdir)
+        self.sdir = newdir
+        self.bdir = newdir
         return self.sys_log("INSTALL_LIB=%s INSTALL_INCLUDE=%s make install" % (self.libdir, self.incdir))
